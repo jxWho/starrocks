27a1e3df980b7a99057ef1e4a6ac75daec374392
Rob Cauble <110474126+rcauble@users.noreply.github.com>
2022-11-15 13:30:37 -0600

    [BugFix]: Use a specific, immovable, hash rather than the tag (#30)
    ## What type of PR is this：
    - [X ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    ## Which issues of this PR fixes ：
    <!--
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    _If PR is about `failing-tests or flakes`, please post the related
    issues/tests in a comment and do not use `Fixes`_*
    -->
    Fixes #
    
    ## Problem Summary(Required) ：
    <!-- (Please describe the changes you have made. In which scenarios will
    this bug be triggered and what measures have you taken to fix the bug?)
    -->
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr will affect users' behaviors
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function

--- celonis/run-in-dev-image.sh
+++ celonis/run-in-dev-image.sh
@@ -6,7 +6,7 @@ set -e
 
 # To update this, find the dev-env version that matches to the base branch version.
 # https://hub.docker.com/r/starrocks/dev-env/tags
-STARROCKS_DEV_IMAGE=starrocks/dev-env:branch-2.5
+STARROCKS_DEV_IMAGE=starrocks/dev-env@sha256:5fca42275926ae2daf4e8666e8c135a76d4a8251d685778d58afd3b46705cc93
 STARROCKS_CONTAINER_PATH=/root/starrocks
 MY_SUBDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 STARROCKS_SUBDIR="$(dirname "$MY_SUBDIR")"
