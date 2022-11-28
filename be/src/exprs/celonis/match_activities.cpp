4574d35d1f617d049d7c54dd17ca92a1bd32aa46
Jiacheng Yang <103014487+jiacheng-celonis@users.noreply.github.com>
2022-11-28 15:16:10 -0800

    [Enhancement] Support NULL arrays input in `celonis_match_activities()` (#47)

--- be/src/exprs/celonis/match_activities.cpp
+++ be/src/exprs/celonis/match_activities.cpp
@@ -1,6 +1,7 @@
 #include "exprs/celonis/match_activities.h"
 
 #include "column/array_column.h"
+#include "column/column_builder.h"
 #include "column/hash_set.h"
 #include "exprs/celonis/util.h"
 #include "exprs/function_context.h"
@@ -9,13 +10,17 @@ namespace starrocks {
 
 namespace {
 
-template<bool has_null>
+template<bool element_has_null>
 ColumnPtr _celonis_match_activities_impl(FunctionContext* context, const Column& elements, const UInt32Column& offsets,
-                                         const NullColumn::Container* null_offsets, const SliceHashSet& nodes) {
+                                         const NullColumn::Container* null_offsets,
+                                         const NullColumn::Container* activity_array_nulls,
+                                         const SliceHashSet& nodes) {
     const size_t num_array = offsets.size() - 1;
     auto offsets_ptr = offsets.get_data().data();
-    auto result_column = Int64Column::create(num_array, 0);
-    int64_t* result_data = result_column->get_data().data();
+
+    ColumnBuilder<TYPE_BIGINT> result(num_array);
+    result.reserve(num_array);
+
     using ValueType = RunTimeCppType<TYPE_VARCHAR>;
     auto elements_ptr = (const ValueType *) (elements.raw_data());
     // Collects the nodes that pass any of the 'NODES' filter.
@@ -23,12 +28,17 @@ ColumnPtr _celonis_match_activities_impl(FunctionContext* context, const Column&
     passing_nodes.reserve(nodes.size());
 
     for (size_t i = 0; i < num_array; i++) {
+        if (activity_array_nulls != nullptr && (*activity_array_nulls)[i]) {
+            result.append_null();
+            continue;
+        }
+
         size_t offset = offsets_ptr[i];
         size_t array_size = offsets_ptr[i + 1] - offsets_ptr[i];
         passing_nodes.clear();
 
         for (size_t index = 0; index < array_size; ++index) {
-            if constexpr (has_null) {
+            if constexpr (element_has_null) {
                 // Nulls are ignored
                 if ((*null_offsets)[offset + index] != 0) {
                     continue;
@@ -45,33 +55,35 @@ ColumnPtr _celonis_match_activities_impl(FunctionContext* context, const Column&
         }
 
         if (passing_nodes.size() == nodes.size()) {
-            result_data[i] = 1;
+            result.append(1L);
+        } else {
+            result.append(0L);
         }
     }
-    return result_column;
+    return result.build(/*is_const=*/false);
 }
 } // namespace
 
 StatusOr<ColumnPtr> CelonisMatchActivitiesFunctions::celonis_match_activities(FunctionContext* context, const Columns& columns) {
-    if (columns[0]->has_null()) {
-        std::stringstream error;
-        error << "unexpected null activities array as input to celonis_match_activities" << std::endl;
-        context->set_error(error.str().c_str());
-        throw std::runtime_error(error.str());
+    const Column* activity_array =  columns[0].get();
+    const NullableColumn* nullable_activity_array = nullptr;
+    const NullColumn::Container* activity_array_nulls = nullptr;
+
+    if (activity_array->is_nullable()) {
+        nullable_activity_array = down_cast<const NullableColumn*>(activity_array);
+        activity_array = nullable_activity_array->data_column().get();
+        activity_array_nulls = &(nullable_activity_array->null_column()->get_data());
     }
 
-    const auto& array_column = extract_array_column(columns[0].get());
-    const UInt32Column& offsets = array_column.offsets();
-    const Column* elements = &array_column.elements();
-    const NullColumn::Container* null_elements = nullptr;
-
-    bool has_null = elements->has_null();
-    if (has_null) {
-        null_elements = &(down_cast<const NullableColumn*>(elements)->null_column()->get_data());
+    const auto& activity_array_column = extract_array_column(activity_array);
+    const UInt32Column& activity_offsets = activity_array_column.offsets();
+    const Column* activity_elements = &activity_array_column.elements();
+    const NullColumn::Container* activity_nulls = nullptr;
+    if (activity_elements->has_null()) {
+        activity_nulls = &(down_cast<const NullableColumn*>(activity_elements)->null_column()->get_data());
     }
-
-    if (auto nullable = dynamic_cast<const NullableColumn*>(elements); nullable != nullptr) {
-        elements = nullable->data_column().get();
+    if (auto nullable = dynamic_cast<const NullableColumn*>(activity_elements); nullable != nullptr) {
+        activity_elements = nullable->data_column().get();
     }
 
     // schema:
@@ -99,10 +111,10 @@ StatusOr<ColumnPtr> CelonisMatchActivitiesFunctions::celonis_match_activities(Fu
         nodes.insert(node_array_col[i].get_slice());
     }
 
-    if (has_null) {
-        _celonis_match_activities_impl<true>(context, *elements, offsets, null_elements, nodes);
+    if (activity_nulls != nullptr) {
+        _celonis_match_activities_impl<true>(context, *activity_elements, activity_offsets, activity_nulls, activity_array_nulls, nodes);
     }
-    return _celonis_match_activities_impl<false>(context, *elements, offsets, null_elements, nodes);
+    return _celonis_match_activities_impl<false>(context, *activity_elements, activity_offsets, activity_nulls, activity_array_nulls, nodes);
 }
 
 } // namespace starrocks
--- be/test/exprs/celonis/match_activities_test.cpp
+++ be/test/exprs/celonis/match_activities_test.cpp
@@ -30,7 +30,7 @@ protected:
     TypeDescriptor TYPE_ARRAY_VARCHAR = array_type(TYPE_VARCHAR);
 };
 
-TEST_F(CelonisMatchActivitiesTest, array_celonis_source_string_data) {
+TEST_F(CelonisMatchActivitiesTest, celonis_match_activities_string_data) {
     std::unique_ptr<FunctionContext> ctx(FunctionContext::create_test_context());
     auto array = ColumnHelper::create_column(TYPE_ARRAY_VARCHAR, false);
     // Input data:
@@ -52,18 +52,19 @@ TEST_F(CelonisMatchActivitiesTest, array_celonis_source_string_data) {
     const auto result = CelonisMatchActivitiesFunctions::celonis_match_activities(
             ctx.get(), {array, other_filters, nodes_filter, other_filters, other_filters, other_filters, other_filters}).value();
     EXPECT_EQ(3, result->size());
-    EXPECT_EQ(0, result->get(0).get_uint8());
-    EXPECT_EQ(1, result->get(1).get_uint8());
-    EXPECT_EQ(1, result->get(2).get_uint8());
+    EXPECT_EQ(0, result->get(0).get_int64());
+    EXPECT_EQ(1, result->get(1).get_int64());
+    EXPECT_EQ(1, result->get(2).get_int64());
 }
 
-TEST_F(CelonisMatchActivitiesTest, array_celonis_source_string_data_nullable) {
+TEST_F(CelonisMatchActivitiesTest, celonis_match_activities_string_data_nullable) {
     // Similar to above but the input array is declared as NULLable.
     std::unique_ptr<FunctionContext> ctx(FunctionContext::create_test_context());
     auto array = ColumnHelper::create_column(TYPE_ARRAY_VARCHAR, true);
     array->append_datum(DatumArray{"string1", "string2"});
     array->append_datum(DatumArray{Datum(), "string1", Datum(), "string2", "string3"});
     array->append_datum(DatumArray{"string3", "string4", "string1"});
+    array->append_nulls(1);
 
     auto nodes_filter = ColumnHelper::create_column(TYPE_ARRAY_VARCHAR, true);
     nodes_filter->append_datum(DatumArray{"string1", "string3"});
@@ -71,14 +72,17 @@ TEST_F(CelonisMatchActivitiesTest, array_celonis_source_string_data_nullable) {
     other_filters->append_datum(DatumArray{});
     const auto result = CelonisMatchActivitiesFunctions::celonis_match_activities(
             ctx.get(), {array, other_filters, nodes_filter, other_filters, other_filters, other_filters, other_filters}).value();
-    EXPECT_EQ(3, result->size());
-    EXPECT_EQ(0, result->get(0).get_uint8());
-    EXPECT_EQ(1, result->get(1).get_uint8());
-    EXPECT_EQ(1, result->get(2).get_uint8());
+    EXPECT_EQ(4, result->size());
+    EXPECT_EQ(0, result->get(0).get_int64());
+    EXPECT_EQ(1, result->get(1).get_int64());
+    EXPECT_EQ(1, result->get(2).get_int64());
+    EXPECT_FALSE(result->get(0).is_null());
+    EXPECT_FALSE(result->get(1).is_null());
+    EXPECT_FALSE(result->get(2).is_null());
+    EXPECT_TRUE(result->get(3).is_null());
 }
 
-
-TEST_F(CelonisMatchActivitiesTest, array_celonis_source_unsupported_filter) {
+TEST_F(CelonisMatchActivitiesTest, celonis_match_activities_unsupported_filter) {
     // Similar to above, but provides node information in one of the filters that is not supported for now ("starting"
     // nodes).
     std::unique_ptr<FunctionContext> ctx(FunctionContext::create_test_context());
