fb084b93f2dd3c21559558463d24f031c68ac4d6
Deng Liu <d.liu@celonis.com>
2023-02-17 08:06:25 -0800

    [Enhancement] Terminate the container when lose BE heartbeat (#121)
    ## What type of PR is this：
    - [ ] BugFix
    - [ ] Feature
    - [X] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    ## Which issues of this PR fixes ：
    The current allin1 container won't exit when Starrocks BE crashes. This
    prevent k8s from auto restarting the container to self-heal itself.
    This PR adds BE heart checking and terminates the main running process
    to trigger a restart of the container by k8s.
    
    
    ## TEST:
    Tested this in a docker compose environment by manually stop the BE for
    a running allin1 container and verified that the container terminated on
    loosing the heartbeat, and get automatically restarted.
    ```
    [Fri Feb 17 00:11:51 UTC 2023] start_be.sh
    [Fri Feb 17 00:12:03 UTC 2023] tcp 0 0 0.0.0.0:9050 0.0.0.0:* LISTEN 1387/starrocks_be
    [Fri Feb 17 00:12:13 UTC 2023] tcp 0 0 0.0.0.0:9050 0.0.0.0:* LISTEN 1387/starrocks_be
    [Fri Feb 17 00:12:23 UTC 2023] BE has exited
    [Fri Feb 17 00:12:27 UTC 2023] start_be.sh
    [Fri Feb 17 00:12:38 UTC 2023] tcp 0 0 0.0.0.0:9050 0.0.0.0:* LISTEN 1397/starrocks_be
    [Fri Feb 17 00:12:48 UTC 2023] tcp 0 0 0.0.0.0:9050 0.0.0.0:* LISTEN 1397/starrocks_be
    [Fri Feb 17 00:12:58 UTC 2023] tcp 0 0 0.0.0.0:9050 0.0.0.0:* LISTEN 1397/starrocks_be
    ```

--- celonis/docker/allin1/start_be.sh
+++ celonis/docker/allin1/start_be.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+log_stdin()
+{
+    echo "[`date`] $@" >&1
+}
 
 # Start BE.
 cd $SR_HOME/be/bin/
@@ -6,6 +10,15 @@ cd $SR_HOME/be/bin/
 # Try stop be if there is an instance
 ./stop_be.sh
 sleep 3
+log_stdin "start_be.sh"
 ./start_be.sh --daemon
 
-sleep infinity
+# Loop to detect the heart beat.
+while sleep 10; do
+  BE_STATUS=`netstat -ltpn | grep 9050`
+  if [ -z "$BE_STATUS" ]; then
+    log_stdin "BE has exited"
+    exit 1;
+  fi;
+  log_stdin $BE_STATUS
+done
