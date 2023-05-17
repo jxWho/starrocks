0766421fe35622988b7a39b9441fa4b70fe4ba2a
Kaiwei Wang <fieldsfarmer@users.noreply.github.com>
2023-05-17 14:30:11 -0700

    Use a new dev env image and add backend unit test in the presubmit test workflow (#213)
    ## What type of PR is this：
    - [x] Tool

--- celonis/docker/presubmit/presubmit_test.sh
+++ celonis/docker/presubmit/presubmit_test.sh
@@ -6,5 +6,8 @@ echo "./build.sh --fe --clean";
 echo "./build.sh --be --clean -j `nproc`";
 ./build.sh --be --clean -j `nproc`;
 
+echo "./run-be-ut.sh";
+./run-be-ut.sh;
+
 echo "mvn clean package -f celonis/udf/duplicate-invoice-checker/pom.xml";
 mvn clean package -f celonis/udf/duplicate-invoice-checker/pom.xml;
