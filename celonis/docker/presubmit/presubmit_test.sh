df2145d54286e5e047cb70d6270f7839af00dc47
Kaiwei Wang <fieldsfarmer@users.noreply.github.com>
2023-05-18 11:01:04 -0700

    Add fe ut test (#215)
    ## What type of PR is this：
    - [x] UT
    - [x] Tool

--- celonis/docker/presubmit/presubmit_test.sh
+++ celonis/docker/presubmit/presubmit_test.sh
@@ -9,5 +9,8 @@ echo "./build.sh --be --clean -j `nproc`";
 echo "./run-be-ut.sh";
 ./run-be-ut.sh;
 
+echo "./run-fe-ut.sh";
+./run-fe-ut.sh;
+
 echo "mvn clean package -f celonis/udf/duplicate-invoice-checker/pom.xml";
 mvn clean package -f celonis/udf/duplicate-invoice-checker/pom.xml;
