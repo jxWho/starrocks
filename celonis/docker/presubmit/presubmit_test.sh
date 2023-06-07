4a146fe1bc4dc4432e76c1dd94c4f9f5902c8753
Kaiwei Wang <fieldsfarmer@users.noreply.github.com>
2023-06-07 14:44:45 -0700

    Enable parallel in unit tests (#231)
    ## What type of PR is this：
    - [x] Tool

--- celonis/docker/presubmit/presubmit_test.sh
+++ celonis/docker/presubmit/presubmit_test.sh
@@ -4,12 +4,13 @@ echo "./build.sh --fe --clean";
 ./build.sh --fe --clean;
 
 echo "./build.sh --be --clean -j `nproc`";
-./build.sh --be --clean -j `nproc`;
+./build.sh --be --use-staros --clean -j `nproc`;
 
 echo "./run-be-ut.sh";
-./run-be-ut.sh;
+./run-be-ut.sh --use-staros --clean -j `nproc`;
 
 echo "./run-fe-ut.sh";
+export FE_UT_PARALLEL=32;
 ./run-fe-ut.sh;
 
 echo "mvn clean package -f celonis/udf/duplicate-invoice-checker/pom.xml";
