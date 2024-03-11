33d506efb9de56a0a8a1301ebcf31506fee1d8c4
Kaiwei Wang <fieldsfarmer@users.noreply.github.com>
2024-03-11 12:55:25 -0700

    Scan FE & BE jars (#799)
    The veracode scan all java jar files from both FE and BE. It takes more
    time but is more completely.
    
    Tested:
    https://github.com/celonis/celostar-starrocks/actions/runs/8236663641/job/22523625217

--- celonis/docker/veracode/static_analysis.sh
+++ celonis/docker/veracode/static_analysis.sh
@@ -7,6 +7,9 @@ cd /celostar-starrocks;
 echo "./build.sh --fe --clean";
 ./build.sh --fe --clean;
 
+echo "./build.sh --be --clean -j `nproc`";
+./build.sh --be --clean -j `nproc`;
+
 # Zip FE
-echo "Start to zip FE jars"
-zip celostar-starrocks-fe.zip ./output/fe/lib/*.jar
+echo "Start to zip jars in output"
+find ./output -name '*.jar' -print -exec zip celostar-starrocks-java.zip {} +
