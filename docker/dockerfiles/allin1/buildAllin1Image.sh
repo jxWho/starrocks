ba9636961df32edb3a5523b86880e3b96f9d2c34
Kaiwei Wang <fieldsfarmer@users.noreply.github.com>
2024-03-29 09:03:00 -0700

    Backport PR #849 (#850)
    This is to backport
    https://github.com/celonis/celostar-starrocks/pull/849.
    After this PR is merged to `kw/trans_3.2_f2acbad2`, then it will later
    merge to `branch-3.2-f2acbad2-celo` with a PR number.

---
+++ docker/dockerfiles/allin1/buildAllin1Image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+RELEASE_VERSION=${RELEASE_VERSION:-"latest"}
+BUILD_TYPE=${BUILD_TYPE:-"Release"}
+BUILDER=${BUILDER:-"ghcr.io/celonis/celostar/starrocks-dev-env:3.1-cs-ca2bfcd3-cpml"}
+
+docker pull $BUILDER
+docker buildx build --force-rm=true --build-arg RELEASE_VERSION=${RELEASE_VERSION} --build-arg BUILD_TYPE=${BUILD_TYPE} --build-arg builder=${BUILDER} -f ../artifacts/artifact.Dockerfile -t starrocks-artifacts:${RELEASE_VERSION} ../../..
+docker buildx build --force-rm=true --build-arg ARTIFACTIMAGE=starrocks-artifacts:${RELEASE_VERSION} -f allin1-ubuntu.Dockerfile -t ghcr.io/celonis/celostar/starrocks-allin1:${RELEASE_VERSION} ../../..
