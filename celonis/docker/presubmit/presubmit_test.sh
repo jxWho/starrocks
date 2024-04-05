5eb04f70d45c3c31596ec5af554e465e631554d0
Kaiwei Wang <fieldsfarmer@users.noreply.github.com>
2024-04-05 14:15:32 -0700

    Run BE UT on Celonis and FE UT in presubmit test (#866)
    While we are working on to run all BE UT, we firstly run all "Celonis"
    related BE UT to prevent regression.
    
    FE UT result:
    ![Screenshot 2024-04-05 at 1 52
    50 PM](https://github.com/celonis/celostar-starrocks/assets/812434/80cf081c-0cd8-40a2-a9f3-002a035ed0c8)
    
    BE UT result:
    ![Screenshot 2024-04-05 at 1 46
    23 PM](https://github.com/celonis/celostar-starrocks/assets/812434/a0fe060e-eca0-446f-b4c1-a383e7efe892)

--- celonis/docker/presubmit/presubmit_test.sh
+++ celonis/docker/presubmit/presubmit_test.sh
@@ -4,17 +4,16 @@ set -e
 
 cd /celostar-starrocks;
 
-echo "./build.sh --fe --clean";
-./build.sh --fe --clean;
+#echo "./build.sh --fe --clean";
+#./build.sh --fe --clean;
+#
+#echo "./build.sh --be --clean -j `nproc`";
+#./build.sh --be --clean -j `nproc`;
 
-echo "./build.sh --be --clean -j `nproc`";
-./build.sh --be --clean -j `nproc`;
+echo "Start to run FE UT"
+export FE_UT_PARALLEL=16;
+./run-fe-ut.sh;
 
-# Put FE & BE UT back after migrating to branch-3.2
-
-#echo "./run-be-ut.sh";
-#./run-be-ut.sh --use-staros --clean -j `nproc`;
-
-#echo "./run-fe-ut.sh";
-#export FE_UT_PARALLEL=32;
-#./run-fe-ut.sh;
+echo "Start to run Celonis BE UT"
+export LD_LIBRARY_PATH=/var/local/thirdparty/installed/lib
+./run-be-ut.sh --use-staros --clean -j `nproc` --test "Celonis*"
