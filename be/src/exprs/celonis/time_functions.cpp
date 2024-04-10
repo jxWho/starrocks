52bbaa544f7b98c308c2ea2dbdf556cf2a3a0985
Yaqi Zhang <y.zhang@celonis.com>
2024-04-10 10:55:20 -0500

    [BugFix] Unpack and duplicate calendar column in general Calendar func implementations (#886)
    Tested locally, this can fix the heap-buffer-overflow issue (repo
    [SQL](https://gist.github.com/zhangyaqi1989/0f03821b75c74357b83f7b9c0eae4c8d)).
    This is a temporary workaround. We are waiting for the fix on SR side.
    
    ## What type of PR is this:
    
    - [x] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [x] Yes, this PR will result in a change in behavior.
    - [ ] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/exprs/celonis/time_functions.cpp
+++ be/src/exprs/celonis/time_functions.cpp
@@ -1262,7 +1262,8 @@ StatusOr<ColumnPtr> remap_timestamps_calendar_general([[maybe_unused]] FunctionC
     ColumnViewer timestamp_viewer = ColumnViewer<TYPE_DATETIME>(columns[0]);
     ColumnViewer time_unit_viewer = ColumnViewer<TYPE_VARCHAR>(columns[1]);
     ColumnViewer calendar_id_viewer = ColumnViewer<TYPE_VARCHAR>(columns[3]);
-    UnnestedArrayData calendar_array_data = prepare_array_input(columns[2].get());
+    UnnestedArrayData calendar_array_data = prepare_array_input(
+            ColumnHelper::unpack_and_duplicate_const_column(columns[2]->size(), columns[2]).get());
     if (calendar_array_data.null_elements != nullptr) {
         return Status::InvalidArgument("Calendar array should not have null elements.");
     }
@@ -1353,7 +1354,8 @@ StatusOr<ColumnPtr> in_calendar_general([[maybe_unused]] FunctionContext* contex
     ColumnViewer timestamp_viewer = ColumnViewer<TYPE_DATETIME>(columns[0]);
     ColumnViewer calendar_id_viewer = ColumnViewer<TYPE_VARCHAR>(columns[2]);
 
-    UnnestedArrayData calendar_array_data = prepare_array_input(columns[1].get());
+    UnnestedArrayData calendar_array_data = prepare_array_input(
+            ColumnHelper::unpack_and_duplicate_const_column(columns[1]->size(), columns[1]).get());
     if (calendar_array_data.null_elements != nullptr) {
         return Status::InvalidArgument("Calendar array can not contain null values.");
     }
@@ -1638,7 +1640,8 @@ StatusOr<ColumnPtr> timeunits_between_calendar_general([[maybe_unused]] Function
     ColumnViewer to_timestamp_viewer = ColumnViewer<TYPE_DATETIME>(columns[1]);
     ColumnViewer time_unit_viewer = ColumnViewer<TYPE_VARCHAR>(columns[2]);
     ColumnViewer calendar_id_viewer = ColumnViewer<TYPE_VARCHAR>(columns[4]);
-    UnnestedArrayData calendar_array_data = prepare_array_input(columns[3].get());
+    UnnestedArrayData calendar_array_data = prepare_array_input(
+            ColumnHelper::unpack_and_duplicate_const_column(columns[3]->size(), columns[3]).get());
     if (calendar_array_data.null_elements != nullptr) {
         return Status::InvalidArgument("Calendar array should not have null elements.");
     }
@@ -1755,7 +1758,8 @@ static StatusOr<ColumnPtr> add_timeunits_calendar_general([[maybe_unused]] Funct
     ColumnViewer add_value_viewer = ColumnViewer<TYPE_BIGINT>(columns[1]);
     ColumnViewer time_unit_viewer = ColumnViewer<TYPE_VARCHAR>(columns[2]);
     ColumnViewer calendar_id_viewer = ColumnViewer<TYPE_VARCHAR>(columns[4]);
-    UnnestedArrayData calendar_array_data = prepare_array_input(columns[3].get());
+    UnnestedArrayData calendar_array_data = prepare_array_input(
+            ColumnHelper::unpack_and_duplicate_const_column(columns[3]->size(), columns[3]).get());
     if (calendar_array_data.null_elements != nullptr) {
         return Status::InvalidArgument("Calendar array should not have null elements.");
     }
