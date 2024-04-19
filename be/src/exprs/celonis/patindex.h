f4e2b51913756b309745a5e52793d56004f41e42
Yaqi Zhang <y.zhang@celonis.com>
2024-04-19 14:24:56 -0500

    [Doc] Fix a comment in patindex.h (#906)
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [x] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/exprs/celonis/patindex.h
+++ be/src/exprs/celonis/patindex.h
@@ -10,7 +10,7 @@ public:
      * @param: [input_string, pattern, occurrence (optional)]
      * @paramType columns: [VARCHAR, VARCHAR, BIGINT]
      * @return: BIGINT
-     * Supports PQL REMAP_VALUES: https://docs.celonis.com/en/patindex.html
+     * Supports PQL PATINDEX: https://docs.celonis.com/en/patindex.html
      */
     DEFINE_VECTORIZED_FN(patindex);
 
