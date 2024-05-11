3915f0101aea0eb92b9aeee1a8ca8c04a5c0269c
Yaqi Zhang <y.zhang@celonis.com>
2024-05-11 11:47:34 -0500

    [Enhancement] Change EdgeHashMap to use phmap::flat_hash_map (VARIANT_STATS) (#972)
    The detailed benchmark result is in
    [doc](https://docs.google.com/document/d/1fV2mQvATO3gscfHi8q8TRRuJWtXzEc-tGYTb9iguNlk/edit).
    In release mode, phmap::flat_hash_map + boost hash function has the best
    performance.
    
    phmap::flat_hash_map + boost hash function is 3x faster than
    std::unordered_map + boost hash function.
    
    - [ ] BugFix
    - [ ] Feature
    - [x] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [x] Yes, this PR will result in a change in behavior.
    - [ ] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    - [x] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/exprs/celonis/agg/variant_stats.h
+++ be/src/exprs/celonis/agg/variant_stats.h
@@ -132,7 +132,7 @@ struct ActivityStats {
 
 class VariantStatsFinalizer : public VariantAggregateFinalizer {
 public:
-    using EdgeHashMap = std::unordered_map<Edge, EdgeStats, HashOnEdge, EqualOnEdge>;
+    using EdgeHashMap = phmap::flat_hash_map<Edge, EdgeStats, HashOnEdge, EqualOnEdge>;
     using EdgeHashSet = phmap::flat_hash_set<Edge, HashOnEdge, EqualOnEdge>;
 
     VariantStatsFinalizer(FunctionContext* ctx, const VariantStatsState& state)
