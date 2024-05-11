a5c3693d35d86f749ea687b1f13d7c6e90b1325f
Yaqi Zhang <y.zhang@celonis.com>
2024-05-11 06:48:40 -0500

    [BugFix] Do not allocate large char array on stack in to_base64_encoded_string (#971)
    In the original implementation, to_base64_encoded_string tries to
    allocate a potentially very large char array on stack which can cause
    overflow.
    
    ## What type of PR is this:
    
    - [x] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [x] Yes, this PR will result in a change in behavior.
    - [ ] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/exprs/celonis/agg/util.cpp
+++ be/src/exprs/celonis/agg/util.cpp
@@ -8,10 +8,10 @@ std::string to_base64_encoded_string(const celonis::accelerator::Calendar &calen
     std::string binary_string;
     calendar_proto.SerializeToString(&binary_string);
     int cipher_len = (size_t) (4.0 * ceil((double) binary_string.length() / 3.0)) + 1;
-    char p[cipher_len];
+    std::string p(cipher_len, '\0');
 
-    int len = base64_encode2((unsigned char *) binary_string.data(), binary_string.length(), (unsigned char *) p);
-    std::string encoded_string(p, len);
+    int len = base64_encode2((unsigned char *) binary_string.data(), binary_string.length(), (unsigned char *) p.data());
+    std::string encoded_string(p.data(), len);
     return encoded_string;
 }
 
