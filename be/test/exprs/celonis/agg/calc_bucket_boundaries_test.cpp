531f9e5921496c904f422da713bb3fee1755d295
Yaqi Zhang <y.zhang@celonis.com>
2024-07-01 20:45:36 -0500

    [Refactor] calc_bucket_boundaries_test (#1091)
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [x] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/test/exprs/celonis/agg/calc_bucket_boundaries_test.cpp
+++ be/test/exprs/celonis/agg/calc_bucket_boundaries_test.cpp
@@ -18,20 +18,20 @@ class ManagedAggrState {
 public:
     ~ManagedAggrState() { _func->destroy(_ctx, _state); }
 
-    static std::unique_ptr<ManagedAggrState> create(FunctionContext *ctx, const AggregateFunction *func) {
+    static std::unique_ptr<ManagedAggrState> create(FunctionContext* ctx, const AggregateFunction* func) {
         return std::make_unique<ManagedAggrState>(ctx, func);
     }
 
     AggDataPtr state() { return _state; }
 
 private:
-    ManagedAggrState(FunctionContext *ctx, const AggregateFunction *func) : _ctx(ctx), _func(func) {
+    ManagedAggrState(FunctionContext* ctx, const AggregateFunction* func) : _ctx(ctx), _func(func) {
         _state = _mem_pool.allocate_aligned(func->size(), func->alignof_size());
         _func->create(_ctx, _state);
     }
 
-    FunctionContext *_ctx;
-    const AggregateFunction *_func;
+    FunctionContext* _ctx;
+    const AggregateFunction* _func;
     MemPool _mem_pool;
     AggDataPtr _state;
 };
@@ -43,6 +43,7 @@ protected:
     CelonisCalcBucketCountBoundariesTest() = default;
 
     void SetUp() override {}
+
     void TearDown() override {}
 
     TypeDescriptor get_return_type(LogicalType logical_type) {
@@ -52,7 +53,7 @@ protected:
     std::unique_ptr<FunctionContext> get_ctx(LogicalType logical_type) {
         std::vector<FunctionContext::TypeDesc> arg_types = {
                 AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(logical_type)), // input
-                AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(TYPE_BIGINT)) // no_lower_bound
+                AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(TYPE_BIGINT))   // count
         };
         auto return_type = AnyValUtil::column_type_to_type_desc(get_return_type(logical_type));
         return std::unique_ptr<FunctionContext>(
@@ -90,17 +91,17 @@ protected:
     RunUpdate(LogicalType logical_type, const DatumArray& input, int count) {
         auto local_ctx = get_ctx(logical_type);
 
-        const AggregateFunction *func =
+        const AggregateFunction* func =
                 get_aggregate_function("celonis_calc_bucket_count_boundaries", logical_type, TYPE_ARRAY, false);
 
         auto input_col = ColumnHelper::create_column(TypeDescriptor::from_logical_type(logical_type), true);
-        for (const auto& datum : input) {
+        for (const auto& datum: input) {
             input_col->append_datum(datum);
         }
 
         auto count_col = ColumnHelper::create_const_column<TYPE_BIGINT>(count, input.size());
 
-        std::vector<const Column *> raw_columns;
+        std::vector<const Column*> raw_columns;
         raw_columns.resize(2);
         raw_columns[0] = input_col.get();
         raw_columns[1] = count_col.get();
@@ -140,6 +141,36 @@ protected:
 
         Evaluate<LT>(result.get(), expected);
     }
+
+    template<LogicalType LT>
+    void RunMergeToNew(const DatumArray& input1, const DatumArray& input2, int count, const Datum& expected) {
+        auto [local_ctx1, state1, func] = RunUpdate(LT, input1, count);
+        auto [local_ctx2, state2, func2] = RunUpdate(LT, input2, count);
+
+        auto local_ctx3 = get_ctx(LT);
+        // Serialize state1 and state2
+        ColumnPtr serde_col = BinaryColumn::create();
+        func->serialize_to_column(local_ctx1.get(), state1->state(), serde_col.get());
+        func->serialize_to_column(local_ctx2.get(), state2->state(), serde_col.get());
+
+        // Merge to a new state
+        auto state3 = ManagedAggrState::create(local_ctx3.get(), func);
+        func->merge(local_ctx3.get(), serde_col.get(), state3->state(), 0);
+        func->merge(local_ctx3.get(), serde_col.get(), state3->state(), 1);
+
+        // Get the result
+        auto result = local_ctx3->create_column(local_ctx3->get_return_type(), false);
+        func->finalize_to_column(local_ctx3.get(), state3->state(), result.get());
+
+        Evaluate<LT>(result.get(), expected);
+    }
+
+    template<LogicalType LT>
+    void Run(const DatumArray& input1, const DatumArray& input2, int count, const Datum& expected) {
+        RunMerge<LT>(input1, input2, count, expected);
+        RunMergeToNew<LT>(input1, input2, count, expected);
+    }
+
     std::vector<std::unique_ptr<MemPool>> mem_pools_;
 };
 
@@ -149,7 +180,7 @@ TEST_F(CelonisCalcBucketCountBoundariesTest, bigint_merge) {
     int count = 10;
     auto expected = DatumArray{1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L};
 
-    RunMerge<TYPE_BIGINT>(input1, input2, count, expected);
+    Run<TYPE_BIGINT>(input1, input2, count, expected);
 }
 
 TEST_F(CelonisCalcBucketCountBoundariesTest, bigint_merge_outlier) {
@@ -167,7 +198,7 @@ TEST_F(CelonisCalcBucketCountBoundariesTest, bigint_merge_outlier) {
     int count = 10;
     auto expected = DatumArray{1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 101L};
 
-    RunMerge<TYPE_BIGINT>(input1, input2, count, expected);
+    Run<TYPE_BIGINT>(input1, input2, count, expected);
 }
 
 TEST_F(CelonisCalcBucketCountBoundariesTest, bigint_one_row) {
@@ -192,40 +223,24 @@ TEST_F(CelonisCalcBucketCountBoundariesTest, bigint_merge_one_row_and_null) {
     int count = 10;
     auto expected = DatumArray{10L, 11L};
 
-    RunMerge<TYPE_BIGINT>(input1, input2, count, expected);
+    Run<TYPE_BIGINT>(input1, input2, count, expected);
 }
 
-TEST_F(CelonisCalcBucketCountBoundariesTest, double) {
-    const LogicalType LT = TYPE_DOUBLE;
+TEST_F(CelonisCalcBucketCountBoundariesTest, double_input) {
     auto input1 = DatumArray{1.0, 2.0, 3.0, 4.0, 5.0};
     auto input2 = DatumArray{6.0, 7.0, 8.0, 9.0, 10.0};
     int count = 10;
     auto expected = DatumArray{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0};
-
-    auto [local_ctx1, state1, func] = RunUpdate(LT, input1, count);
-    auto [local_ctx2, state2, func2] = RunUpdate(LT, input2, count);
-
-    // Serialize state2
-    ColumnPtr serde_col = BinaryColumn::create();
-    func->serialize_to_column(local_ctx2.get(), state2->state(), serde_col.get());
-
-    // Merge state2 into state1
-    func->merge(local_ctx1.get(), serde_col.get(), state1->state(), 0);
-
-    // Get the result
-    auto result = ColumnHelper::create_column(get_return_type(LT), true);
-    func->finalize_to_column(local_ctx1.get(), state1->state(), result.get());
-
-    Evaluate<LT>(result.get(), expected);
+    Run<TYPE_DOUBLE>(input1, input2, count, expected);
 }
 
 TEST_F(CelonisCalcBucketCountBoundariesTest, double_merge_null_and_one_row) {
     auto input1 = DatumArray{kNullDatum};
-    auto input2 = DatumArray{123.};
+    auto input2 = DatumArray{123.0};
     int count = 10;
-    auto expected = DatumArray{123., 124.};
+    auto expected = DatumArray{123.0, 124.0};
 
-    RunMerge<TYPE_DOUBLE>(input1, input2, count, expected);
+    Run<TYPE_DOUBLE>(input1, input2, count, expected);
 }
 
 TEST_F(CelonisCalcBucketCountBoundariesTest, datetime_merge_null_and_one_row) {
@@ -235,7 +250,7 @@ TEST_F(CelonisCalcBucketCountBoundariesTest, datetime_merge_null_and_one_row) {
     auto expected = DatumArray{TimestampValue::create(2024, 1, 2, 3, 3, 28),
                                TimestampValue::create(2024, 1, 2, 3, 4, 6)};
 
-    RunMerge<TYPE_DATETIME>(input1, input2, count, expected);
+    Run<TYPE_DATETIME>(input1, input2, count, expected);
 }
 
 // TODO(j.kim): Add more DATETIME tests
