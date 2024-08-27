8f610be7277810aa51ab8b217cd25eca2fa18c74
Yaqi Zhang <y.zhang@celonis.com>
2024-08-27 11:29:03 -0500

    [Doc] Update CELONIS_ARRAY_FIRST/LAST comments (#1220)
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [x] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/exprs/celonis/array_end_finder.h
+++ be/src/exprs/celonis/array_end_finder.h
@@ -11,14 +11,16 @@ public:
     /**
      * @param: [input_array]
      * @paramType columns: [ARRAY_VARCHAR | ARRAY_INT | ARRAY_BIGINT | ARRAY_DOUBLE | ARRAY_DATETIME]
-     * @return: Same as the element type of the input array.
+     * @return: The first non-NULL value encountered in the array. The return type matches the element type of the input
+     * array. If the input array is empty or contains only NULL values, returns NULL.
      */
     DEFINE_VECTORIZED_FN(array_first);
 
     /**
      * @param: [input_array]
      * @paramType columns: [ARRAY_VARCHAR | ARRAY_INT | ARRAY_BIGINT | ARRAY_DOUBLE | ARRAY_DATETIME]
-     * @return: Same as the element type of the input array.
+     * @return: The last non-NULL value encountered in the array. The return type matches the element type of the input
+     * array. If the input array is empty or contains only NULL values, returns NULL.
      */
     DEFINE_VECTORIZED_FN(array_last);
 };
