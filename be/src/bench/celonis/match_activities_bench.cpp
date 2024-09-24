3790da55e1b6fa6a239ac30ffd5c6b2f2431d80c
Yaqi Zhang <y.zhang@celonis.com>
2024-09-24 09:52:51 -0500

    [Tool] Extend benchmark of CELONIS_MATCH_ACTIVITIES (#1285)
    This PR extends the benchmark of match_activities to support all the
    match arrays.
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [x] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/bench/celonis/match_activities_bench.cpp
+++ be/src/bench/celonis/match_activities_bench.cpp
@@ -13,58 +13,77 @@
 namespace starrocks {
 
 /*
-2024-09-23T20:36:00+00:00
+2024-09-24T11:38:13+00:00
 Running ./be/build_Release/src/bench/celonis/output/match_activities_bench
-Run on (32 X 3599.97 MHz CPU s)
+Run on (32 X 2650 MHz CPU s)
 CPU Caches:
   L1 Data 32 KiB (x16)
   L1 Instruction 32 KiB (x16)
   L2 Unified 512 KiB (x16)
   L3 Unified 32768 KiB (x2)
-Load Average: 0.00, 1.30, 2.43
-----------------------------------------------------------------------------------------------------------------------------
-Benchmark                                                                  Time             CPU   Iterations UserCounters...
-----------------------------------------------------------------------------------------------------------------------------
-BM_MatchActivitiesNonConstantConfig/1000/20/20/5                     1166005 ns      1165867 ns          601 RowInvRate=1.16587us
-BM_MatchActivitiesNonConstantConfig/10000/20/20/5                   11604390 ns     11603537 ns           60 RowInvRate=1.16035us
-BM_MatchActivitiesNonConstantConfig/100000/20/20/5                 133645470 ns    133637089 ns            5 RowInvRate=1.33637us
-BM_MatchActivitiesNonConstantConfig/1000/20/40/5                     1112003 ns      1111643 ns          629 RowInvRate=1.11164us
-BM_MatchActivitiesNonConstantConfig/10000/20/40/5                   11057622 ns     11056939 ns           64 RowInvRate=1.10569us
-BM_MatchActivitiesNonConstantConfig/100000/20/40/5                 127435574 ns    127419560 ns            5 RowInvRate=1.2742us
-BM_MatchActivitiesNonConstantConfig/1000/20/60/5                     1091478 ns      1091199 ns          642 RowInvRate=1091.2ns
-BM_MatchActivitiesNonConstantConfig/10000/20/60/5                   10854067 ns     10853080 ns           65 RowInvRate=1085.31ns
-BM_MatchActivitiesNonConstantConfig/100000/20/60/5                 124702094 ns    124688471 ns            6 RowInvRate=1.24688us
-BM_MatchActivitiesNonConstantConfig/1000/20/20/10                    1817542 ns      1817340 ns          385 RowInvRate=1.81734us
-BM_MatchActivitiesNonConstantConfig/10000/20/20/10                  18079852 ns     18077560 ns           39 RowInvRate=1.80776us
-BM_MatchActivitiesNonConstantConfig/100000/20/20/10                198308974 ns    198282187 ns            4 RowInvRate=1.98282us
-BM_MatchActivitiesNonConstantConfig/1000/20/40/10                    1757381 ns      1756812 ns          399 RowInvRate=1.75681us
-BM_MatchActivitiesNonConstantConfig/10000/20/40/10                  17417547 ns     17411405 ns           40 RowInvRate=1.74114us
-BM_MatchActivitiesNonConstantConfig/100000/20/40/10                192156728 ns    192132620 ns            4 RowInvRate=1.92133us
-BM_MatchActivitiesNonConstantConfig/1000/20/60/10                    1716876 ns      1716438 ns          409 RowInvRate=1.71644us
-BM_MatchActivitiesNonConstantConfig/10000/20/60/10                  17084635 ns     17075674 ns           41 RowInvRate=1.70757us
-BM_MatchActivitiesNonConstantConfig/100000/20/60/10                188902485 ns    188870290 ns            4 RowInvRate=1.8887us
-BM_MatchActivitiesConstantConfig/1000/20/20/5                         138958 ns       138882 ns         5024 RowInvRate=138.882ns
-BM_MatchActivitiesConstantConfig/10000/20/20/5                       1406058 ns      1405904 ns          488 RowInvRate=140.59ns
-BM_MatchActivitiesConstantConfig/100000/20/20/5                     29848575 ns     29844931 ns           23 RowInvRate=298.449ns
-BM_MatchActivitiesConstantConfig/1000/20/40/5                          92585 ns        92551 ns         7594 RowInvRate=92.5507ns
-BM_MatchActivitiesConstantConfig/10000/20/40/5                        917723 ns       917567 ns          773 RowInvRate=91.7567ns
-BM_MatchActivitiesConstantConfig/100000/20/40/5                     25050892 ns     25047317 ns           27 RowInvRate=250.473ns
-BM_MatchActivitiesConstantConfig/1000/20/60/5                          76650 ns        76645 ns         9078 RowInvRate=76.6446ns
-BM_MatchActivitiesConstantConfig/10000/20/60/5                        759684 ns       759606 ns          962 RowInvRate=75.9606ns
-BM_MatchActivitiesConstantConfig/100000/20/60/5                     23642733 ns     23640121 ns           29 RowInvRate=236.401ns
-BM_MatchActivitiesConstantConfig/1000/20/20/10                        209224 ns       209162 ns         3359 RowInvRate=209.162ns
-BM_MatchActivitiesConstantConfig/10000/20/20/10                      2103466 ns      2103234 ns          346 RowInvRate=210.323ns
-BM_MatchActivitiesConstantConfig/100000/20/20/10                    35831964 ns     35824480 ns           19 RowInvRate=358.245ns
-BM_MatchActivitiesConstantConfig/1000/20/40/10                        131073 ns       131059 ns         5219 RowInvRate=131.059ns
-BM_MatchActivitiesConstantConfig/10000/20/40/10                      1321689 ns      1321497 ns          535 RowInvRate=132.15ns
-BM_MatchActivitiesConstantConfig/100000/20/40/10                    28683960 ns     28680140 ns           24 RowInvRate=286.801ns
-BM_MatchActivitiesConstantConfig/1000/20/60/10                        104207 ns       104208 ns         6688 RowInvRate=104.208ns
-BM_MatchActivitiesConstantConfig/10000/20/60/10                      1020608 ns      1020465 ns          673 RowInvRate=102.047ns
-BM_MatchActivitiesConstantConfig/100000/20/60/10                    26168236 ns     26159972 ns           27 RowInvRate=261.6ns
-BM_MatchActivitiesConstantLargeMatchesConfig/1000/20/5000/3000        486821 ns       486707 ns         1433 RowInvRate=486.707ns
-BM_MatchActivitiesConstantLargeMatchesConfig/10000/20/5000/3000      1871370 ns      1870894 ns          380 RowInvRate=187.089ns
-BM_MatchActivitiesConstantLargeMatchesConfig/100000/20/5000/3000    31855664 ns     31851125 ns           22 RowInvRate=318.511ns
-BM_MatchActivitiesConstantLargeMatchesConfig/1000000/20/5000/3000  334182841 ns    334141508 ns            2 RowInvRate=334.142ns
+Load Average: 1.30, 2.24, 2.02
+Args: Number of rows/ Length of each variant / Number of possible values / Starting nodes length / Nodes length / Ending nodes length / Excluding nodes length / Excluding all nodes length / Any nodes length
+--------------------------------------------------------------------------------------------------------------------------------------
+Benchmark                                                                            Time             CPU   Iterations UserCounters...
+--------------------------------------------------------------------------------------------------------------------------------------
+BM_MatchActivitiesNonConstantConfig/1000/20/20/5/0/0/0/0/0                      726575 ns       726380 ns          962 RowInvRate=726.38ns
+BM_MatchActivitiesNonConstantConfig/10000/20/20/5/0/0/0/0/0                    7203587 ns      7201647 ns           94 RowInvRate=720.165ns
+BM_MatchActivitiesNonConstantConfig/100000/20/20/5/0/0/0/0/0                  88331267 ns     88320049 ns            8 RowInvRate=883.2ns
+BM_MatchActivitiesNonConstantConfig/1000/20/40/5/0/0/0/0/0                      673721 ns       673427 ns         1038 RowInvRate=673.427ns
+BM_MatchActivitiesNonConstantConfig/10000/20/40/5/0/0/0/0/0                    6725705 ns      6725238 ns          104 RowInvRate=672.524ns
+BM_MatchActivitiesNonConstantConfig/100000/20/40/5/0/0/0/0/0                  83463436 ns     83451618 ns            8 RowInvRate=834.516ns
+BM_MatchActivitiesNonConstantConfig/1000/20/60/5/0/0/0/0/0                      654212 ns       654025 ns         1070 RowInvRate=654.025ns
+BM_MatchActivitiesNonConstantConfig/10000/20/60/5/0/0/0/0/0                    6496427 ns      6495883 ns          106 RowInvRate=649.588ns
+BM_MatchActivitiesNonConstantConfig/100000/20/60/5/0/0/0/0/0                  81662924 ns     81652008 ns            8 RowInvRate=816.52ns
+BM_MatchActivitiesNonConstantConfig/1000/20/20/10/0/0/0/0/0                    1092171 ns      1091717 ns          635 RowInvRate=1091.72ns
+BM_MatchActivitiesNonConstantConfig/10000/20/20/10/0/0/0/0/0                  10877792 ns     10874808 ns           63 RowInvRate=1087.48ns
+BM_MatchActivitiesNonConstantConfig/100000/20/20/10/0/0/0/0/0                125288166 ns    125274205 ns            6 RowInvRate=1.25274us
+BM_MatchActivitiesNonConstantConfig/1000/20/40/10/0/0/0/0/0                    1024559 ns      1024077 ns          684 RowInvRate=1024.08ns
+BM_MatchActivitiesNonConstantConfig/10000/20/40/10/0/0/0/0/0                  10212568 ns     10211522 ns           69 RowInvRate=1021.15ns
+BM_MatchActivitiesNonConstantConfig/100000/20/40/10/0/0/0/0/0                118298309 ns    118285106 ns            6 RowInvRate=1.18285us
+BM_MatchActivitiesNonConstantConfig/1000/20/60/10/0/0/0/0/0                     988242 ns       987667 ns          706 RowInvRate=987.667ns
+BM_MatchActivitiesNonConstantConfig/10000/20/60/10/0/0/0/0/0                   9824092 ns      9823166 ns           71 RowInvRate=982.317ns
+BM_MatchActivitiesNonConstantConfig/100000/20/60/10/0/0/0/0/0                115321093 ns    115306740 ns            6 RowInvRate=1.15307us
+BM_MatchActivitiesConstantConfig/1000/20/20/5/0/0/0/0/0                         134706 ns       134596 ns         5184 RowInvRate=134.596ns
+BM_MatchActivitiesConstantConfig/10000/20/20/5/0/0/0/0/0                       1351604 ns      1351493 ns          517 RowInvRate=135.149ns
+BM_MatchActivitiesConstantConfig/100000/20/20/5/0/0/0/0/0                     29753627 ns     29749527 ns           24 RowInvRate=297.495ns
+BM_MatchActivitiesConstantConfig/1000/20/40/5/0/0/0/0/0                          91427 ns        91362 ns         7690 RowInvRate=91.3621ns
+BM_MatchActivitiesConstantConfig/10000/20/40/5/0/0/0/0/0                        901323 ns       901170 ns          796 RowInvRate=90.117ns
+BM_MatchActivitiesConstantConfig/100000/20/40/5/0/0/0/0/0                     25375193 ns     25370943 ns           28 RowInvRate=253.709ns
+BM_MatchActivitiesConstantConfig/1000/20/60/5/0/0/0/0/0                          76023 ns        75925 ns         9202 RowInvRate=75.9245ns
+BM_MatchActivitiesConstantConfig/10000/20/60/5/0/0/0/0/0                        761828 ns       761703 ns          958 RowInvRate=76.1703ns
+BM_MatchActivitiesConstantConfig/100000/20/60/5/0/0/0/0/0                     23931778 ns     23929029 ns           29 RowInvRate=239.29ns
+BM_MatchActivitiesConstantConfig/1000/20/20/10/0/0/0/0/0                        202319 ns       202184 ns         3459 RowInvRate=202.184ns
+BM_MatchActivitiesConstantConfig/10000/20/20/10/0/0/0/0/0                      1992472 ns      1992076 ns          353 RowInvRate=199.208ns
+BM_MatchActivitiesConstantConfig/100000/20/20/10/0/0/0/0/0                    35394120 ns     35390691 ns           20 RowInvRate=353.907ns
+BM_MatchActivitiesConstantConfig/1000/20/40/10/0/0/0/0/0                        129392 ns       129313 ns         5453 RowInvRate=129.313ns
+BM_MatchActivitiesConstantConfig/10000/20/40/10/0/0/0/0/0                      1288357 ns      1288166 ns          561 RowInvRate=128.817ns
+BM_MatchActivitiesConstantConfig/100000/20/40/10/0/0/0/0/0                    29021055 ns     29018435 ns           24 RowInvRate=290.184ns
+BM_MatchActivitiesConstantConfig/1000/20/60/10/0/0/0/0/0                        102465 ns       102407 ns         6812 RowInvRate=102.407ns
+BM_MatchActivitiesConstantConfig/10000/20/60/10/0/0/0/0/0                       995884 ns       995783 ns          702 RowInvRate=99.5783ns
+BM_MatchActivitiesConstantConfig/100000/20/60/10/0/0/0/0/0                    26399498 ns     26397671 ns           27 RowInvRate=263.977ns
+BM_MatchActivitiesConstantLargeMatchesConfig/1000/20/5000/3000/0/0/0/0/0        306314 ns       306212 ns         2287 RowInvRate=306.212ns
+BM_MatchActivitiesConstantLargeMatchesConfig/10000/20/5000/3000/0/0/0/0/0      1548053 ns      1547787 ns          450 RowInvRate=154.779ns
+BM_MatchActivitiesConstantLargeMatchesConfig/100000/20/5000/3000/0/0/0/0/0    30829554 ns     30824642 ns           23 RowInvRate=308.246ns
+BM_MatchActivitiesConstantLargeMatchesConfig/1000000/20/5000/3000/0/0/0/0/0  320120132 ns    320080867 ns            2 RowInvRate=320.081ns
+BM_MatchActivitiesConstantConfig/1000/20/20/0/0/0/0/5/0                         613645 ns       613431 ns         1141 RowInvRate=613.431ns
+BM_MatchActivitiesConstantConfig/10000/20/20/0/0/0/0/5/0                       6151608 ns      6151042 ns          112 RowInvRate=615.104ns
+BM_MatchActivitiesConstantConfig/100000/20/20/0/0/0/0/5/0                     77379160 ns     77372583 ns            9 RowInvRate=773.726ns
+BM_MatchActivitiesConstantConfig/1000/20/40/0/0/0/0/5/0                         501209 ns       501138 ns         1386 RowInvRate=501.138ns
+BM_MatchActivitiesConstantConfig/10000/20/40/0/0/0/0/5/0                       5016032 ns      5015556 ns          141 RowInvRate=501.556ns
+BM_MatchActivitiesConstantConfig/100000/20/40/0/0/0/0/5/0                     66300531 ns     66293244 ns           10 RowInvRate=662.932ns
+BM_MatchActivitiesConstantConfig/1000/20/60/0/0/0/0/5/0                         450142 ns       449956 ns         1557 RowInvRate=449.956ns
+BM_MatchActivitiesConstantConfig/10000/20/60/0/0/0/0/5/0                       4495812 ns      4495365 ns          155 RowInvRate=449.537ns
+BM_MatchActivitiesConstantConfig/100000/20/60/0/0/0/0/5/0                     60996406 ns     60990367 ns           11 RowInvRate=609.904ns
+BM_MatchActivitiesConstantConfig/1000/20/20/0/0/0/0/10/0                        760417 ns       760318 ns          915 RowInvRate=760.318ns
+BM_MatchActivitiesConstantConfig/10000/20/20/0/0/0/0/10/0                      7621121 ns      7618448 ns           92 RowInvRate=761.845ns
+BM_MatchActivitiesConstantConfig/100000/20/20/0/0/0/0/10/0                    91024484 ns     91013275 ns            8 RowInvRate=910.133ns
+BM_MatchActivitiesConstantConfig/1000/20/40/0/0/0/0/10/0                        612922 ns       612650 ns         1143 RowInvRate=612.65ns
+BM_MatchActivitiesConstantConfig/10000/20/40/0/0/0/0/10/0                      6156512 ns      6155497 ns          110 RowInvRate=615.55ns
+BM_MatchActivitiesConstantConfig/100000/20/40/0/0/0/0/10/0                    75680465 ns     75672830 ns            9 RowInvRate=756.728ns
+BM_MatchActivitiesConstantConfig/1000/20/60/0/0/0/0/10/0                        531754 ns       531545 ns         1312 RowInvRate=531.545ns
+BM_MatchActivitiesConstantConfig/10000/20/60/0/0/0/0/10/0                      5276872 ns      5276551 ns          128 RowInvRate=527.655ns
+BM_MatchActivitiesConstantConfig/100000/20/60/0/0/0/0/10/0                    69519924 ns     69506767 ns           10 RowInvRate=695.068ns
 */
 
 enum MatchType {
@@ -76,7 +95,14 @@ static void do_bench(benchmark::State& state, MatchType match_type) {
     int num_rows = state.range(0);
     int variant_length = state.range(1);
     int num_values = state.range(2);
-    int match_size = state.range(3);
+    // STARTING, NODE, ENDING, EXCLUDING, EXCLUDING_ALL, NODES_ANY
+    int starting_nodes_length = state.range(3);
+    int nodes_length = state.range(4);
+    int ending_nodes_length = state.range(5);
+    int excluding_nodes_length = state.range(6);
+    int excluding_all_nodes_length = state.range(7);
+    int nodes_any_length = state.range(8);
+
 
     using UniformInt = std::uniform_int_distribution<std::mt19937::result_type>;
     std::random_device dev;
@@ -133,24 +159,24 @@ static void do_bench(benchmark::State& state, MatchType match_type) {
                 TypeDescriptor::create_array_type(TypeDescriptor::create_varchar_type(20)), false);
         switch (match_type) {
             case CONSTANT:
-                starting_nodes_column->append_datum(gen_rand_array(match_size));
-                nodes_column->append_datum(gen_rand_array(0));
-                ending_nodes_column->append_datum(gen_rand_array(match_size));
-                excluding_nodes_column->append_datum(gen_rand_array(0));
-                excluding_all_nodes_column->append_datum(gen_rand_array(0));
-                any_nodes_column->append_datum(gen_rand_array(0));
+                starting_nodes_column->append_datum(gen_rand_array(starting_nodes_length));
+                nodes_column->append_datum(gen_rand_array(nodes_length));
+                ending_nodes_column->append_datum(gen_rand_array(ending_nodes_length));
+                excluding_nodes_column->append_datum(gen_rand_array(excluding_nodes_length));
+                excluding_all_nodes_column->append_datum(gen_rand_array(excluding_all_nodes_length));
+                any_nodes_column->append_datum(gen_rand_array(nodes_any_length));
                 ctx->set_constant_columns(
                         {nullptr, starting_nodes_column, nodes_column, ending_nodes_column, excluding_nodes_column,
                          excluding_all_nodes_column, any_nodes_column});
                 break;
             case NON_CONSTANT:
                 for (int i = 0; i < num_rows; i++) {
-                    starting_nodes_column->append_datum(gen_rand_array(match_size));
-                    nodes_column->append_datum(gen_rand_array(0));
-                    ending_nodes_column->append_datum(gen_rand_array(match_size));
-                    excluding_nodes_column->append_datum(gen_rand_array(0));
-                    excluding_all_nodes_column->append_datum(gen_rand_array(0));
-                    any_nodes_column->append_datum(gen_rand_array(0));
+                    starting_nodes_column->append_datum(gen_rand_array(starting_nodes_length));
+                    nodes_column->append_datum(gen_rand_array(nodes_length));
+                    ending_nodes_column->append_datum(gen_rand_array(ending_nodes_length));
+                    excluding_nodes_column->append_datum(gen_rand_array(excluding_nodes_length));
+                    excluding_all_nodes_column->append_datum(gen_rand_array(excluding_all_nodes_length));
+                    any_nodes_column->append_datum(gen_rand_array(nodes_any_length));
                 }
                 ctx->set_constant_columns({nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr});
                 break;
@@ -184,10 +210,13 @@ static void BM_MatchActivitiesConstantLargeMatchesConfig(benchmark::State& state
     do_bench(state, CONSTANT);
 }
 
-// Args: Number of rows/ Length of each variant / Number of possible values / Size of match list
-BENCHMARK(BM_MatchActivitiesNonConstantConfig)->ArgsProduct({{1000, 10000, 100000}, {20}, {20, 40, 60}, {5, 10}});
-BENCHMARK(BM_MatchActivitiesConstantConfig)->ArgsProduct({{1000, 10000, 100000}, {20}, {20, 40, 60}, {5, 10}});
-BENCHMARK(BM_MatchActivitiesConstantLargeMatchesConfig)->ArgsProduct({{1000, 10000, 100000, 1000000}, {20}, {5000}, {3000}});
+// Args: Number of rows/ Length of each variant / Number of possible values / Starting nodes length / Nodes length / Ending nodes length / Excluding nodes length / Excluding all nodes length / Any nodes length
+// Starting nodes
+BENCHMARK(BM_MatchActivitiesNonConstantConfig)->ArgsProduct({{1000, 10000, 100000}, {20}, {20, 40, 60}, {5, 10}, {0}, {0}, {0}, {0}, {0}});
+BENCHMARK(BM_MatchActivitiesConstantConfig)->ArgsProduct({{1000, 10000, 100000}, {20}, {20, 40, 60}, {5, 10}, {0}, {0}, {0}, {0}, {0}});
+BENCHMARK(BM_MatchActivitiesConstantLargeMatchesConfig)->ArgsProduct({{1000, 10000, 100000, 1000000}, {20}, {5000}, {3000}, {0}, {0}, {0}, {0}, {0}});
+// Excluding all nodes
+BENCHMARK(BM_MatchActivitiesConstantConfig)->ArgsProduct({{1000, 10000, 100000}, {20}, {20, 40, 60}, {0}, {0}, {0}, {0}, {5, 10}, {0}});
 
 } // namespace starrocks
 
