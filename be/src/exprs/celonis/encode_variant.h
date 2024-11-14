25d05c9242f58d96823406018e3b226bc5528852
Yaqi Zhang <y.zhang@celonis.com>
2024-11-13 20:30:28 -0600

    [Doc] Fix an error in CELONIS_ENCODE_VARIANT's comment (#1371)
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [x] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/exprs/celonis/encode_variant.h
+++ be/src/exprs/celonis/encode_variant.h
@@ -12,10 +12,12 @@ public:
      * @paramType columns: [ARRAY_VARCHAR, ARRAY_VARCHAR]
      * @return: ARRAY_INT
      * This function expects activity_array column is constant (it also handles that case that activity_array column is
-     * not constant) and uses it to create an activity to int map which is used to encode the variants.
-     * activity_to_index = {activity: idx for idx, activity in enumerate(activity_array)}
+     * not constant) and uses it to create an activity to index (int) map which is used to encode the variants.
+     * Before computing the map, we first dedup the activity_array and remove the NULLs.
+     * For example activity_array = ['A', 'B', NULL, 'D', 'A'], deduped_activity_array = ['A', 'B', 'D']
+     * activity_to_index = {activity: idx for idx, activity in enumerate(deduped_activity_array)}
      * During encoding the variants, NULL activities are ignored. If an activity does not exist in the activity map, it
-     * is mapped to -1. For example variant = ['B', 'A', NULL, 'A', 'W'], activity_array = ['A', 'B', 'C', 'D']
+     * is mapped to -1. For example variant = ['B', 'A', NULL, 'A', 'W'], activity_array = ['A', 'B', NULL, 'D', 'A']
      * encoded_variant = [1, 0, 0, -1].
      * If variant = NULL, encoded_variant = NULL;
      * If activity_array = NULL, encoded_variant = NULL;
