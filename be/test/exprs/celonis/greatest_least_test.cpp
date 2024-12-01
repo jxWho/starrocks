4b2c7f1da2889f03d8bcba05f72ea9de167a9e6e
Yaqi Zhang <y.zhang@celonis.com>
2024-11-30 19:38:44 -0600

    [Refactor] Refactor unit tests of CELONIS_LEAST and CELONIS_GREATEST (#1401)
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [x] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/test/exprs/celonis/greatest_least_test.cpp
+++ be/test/exprs/celonis/greatest_least_test.cpp
@@ -23,7 +23,7 @@ private:
         assert(!types.empty());
         std::vector<FunctionContext::TypeDesc> arg_types{};
 
-        for (const LogicalType type : types) {
+        for (const LogicalType type: types) {
             arg_types.push_back(AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(type)));
             input_columns.push_back(ColumnHelper::create_column(TypeDescriptor(type), true));
         }
@@ -39,7 +39,7 @@ private:
     void AddRow(const DatumArray& values) {
         assert(values.size() == input_columns.size());
         std::size_t col_idx{0};
-        for (auto& column : input_columns) {
+        for (auto& column: input_columns) {
             column->append_datum(values.at(col_idx++));
         }
     }
@@ -57,23 +57,40 @@ private:
     Columns input_columns;
 };
 
-template <typename T>
-struct expected {
+template<typename T>
+struct Expected {
     std::size_t size;
     std::vector<bool> nulls;
     std::vector<T> values;
 };
 
 template<typename T>
-[[nodiscard]] expected<T> make_expected(std::initializer_list<Datum> values) {
-    expected<T> ret{.size = values.size()};
-    for (const auto& datum : values) {
+[[nodiscard]] Expected<T> make_expected(std::initializer_list<Datum> values) {
+    Expected<T> ret{.size = values.size()};
+    for (const auto& datum: values) {
         const bool is_null{datum.is_null()};
         ret.nulls.push_back(is_null);
         ret.values.push_back(is_null ? T{} : datum.get<T>());
     }
     assert(ret.size == ret.nulls.size() && ret.size == ret.values.size());
     return ret;
+}
+
+template<typename T>
+void validate(const ColumnPtr& result, const Expected<T>& expected) {
+    const auto [expected_size, expected_nulls, expected_values] = expected;
+
+    ASSERT_EQ(expected_size, result->size());
+    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
+        const bool expected_is_null{expected_nulls.at(row_idx)};
+        const bool actual_is_null{result->get(row_idx).is_null()};
+        EXPECT_EQ(expected_is_null, actual_is_null);
+        if (!actual_is_null) {
+            const auto& expected_value{expected_values.at(row_idx)};
+            const auto actual_value{result->get(row_idx).get<T>()};
+            EXPECT_EQ(expected_value, actual_value);
+        }
+    }
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_least_single_argument) {
@@ -84,23 +101,13 @@ TEST_F(CelonisGreatestLeastTest, celonis_least_single_argument) {
     AddRow({1L});
     AddRow({kNullDatum});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({1L, kNullDatum})};
+    const auto expected{make_expected<T>({1L, kNullDatum})};
 
     // WHEN
     const auto result{RunLeast().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_greatest_single_argument) {
@@ -111,23 +118,13 @@ TEST_F(CelonisGreatestLeastTest, celonis_greatest_single_argument) {
     AddRow({1.5});
     AddRow({kNullDatum});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({1.5, kNullDatum})};
+    const auto expected = make_expected<T>({1.5, kNullDatum});
 
     // WHEN
     const auto result{RunGreatest().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_greatest_int_data_mixed_nulls) {
@@ -142,23 +139,13 @@ TEST_F(CelonisGreatestLeastTest, celonis_greatest_int_data_mixed_nulls) {
     AddRow({kNullDatum, kNullDatum, kNullDatum});
     AddRow({1L, 1L, 1L});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({3L, 2L, 2L, 3L, kNullDatum, 1L})};
+    const auto expected = make_expected<T>({3L, 2L, 2L, 3L, kNullDatum, 1L});
 
     // WHEN
     const auto result{RunGreatest().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_greatest_double_data_mixed_nulls) {
@@ -166,33 +153,23 @@ TEST_F(CelonisGreatestLeastTest, celonis_greatest_double_data_mixed_nulls) {
     using T = double;
     Prepare(TYPE_DOUBLE, /* number_of_columns */ 3);
 
-    AddRow({1., 2., 3.});
-    AddRow({kNullDatum, 1., 2.});
-    AddRow({2., kNullDatum, 1.});
-    AddRow({3., 2., kNullDatum});
+    AddRow({1.2, 2.9, 3.5});
+    AddRow({kNullDatum, 1.2, 2.4});
+    AddRow({2.0, kNullDatum, 1.0});
+    AddRow({3.0, 2.0, kNullDatum});
     AddRow({kNullDatum, kNullDatum, kNullDatum});
-    AddRow({1., 1., 1.});
+    AddRow({1.0, 1.0, 1.0});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({3., 2., 2., 3., kNullDatum, 1.})};
+    const auto expected = make_expected<T>({3.5, 2.4, 2.0, 3.0, kNullDatum, 1.0});
 
     // WHEN
     const auto result{RunGreatest().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
-[[nodiscard]] inline auto operator""_ts(const unsigned long long seconds) -> TimestampValue {
+[[nodiscard]] inline auto operator ""_ts(const unsigned long long seconds) -> TimestampValue {
     return TimestampValue::MIN_TIMESTAMP_VALUE.add<SECOND>(seconds);
 }
 
@@ -208,23 +185,13 @@ TEST_F(CelonisGreatestLeastTest, celonis_greatest_datetime_data_mixed_nulls) {
     AddRow({kNullDatum, kNullDatum, kNullDatum});
     AddRow({1_ts, 1_ts, 1_ts});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({3_ts, 2_ts, 2_ts, 3_ts, kNullDatum, 1_ts})};
+    const auto expected = make_expected<T>({3_ts, 2_ts, 2_ts, 3_ts, kNullDatum, 1_ts});
 
     // WHEN
     const auto result{RunGreatest().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_greatest_varchar_data_mixed_nulls) {
@@ -239,23 +206,13 @@ TEST_F(CelonisGreatestLeastTest, celonis_greatest_varchar_data_mixed_nulls) {
     AddRow({kNullDatum, kNullDatum, kNullDatum});
     AddRow({"1", "1", "1"});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({"3", "2", "2", "3", kNullDatum, "1"})};
+    const auto expected = make_expected<T>({"3", "2", "2", "3", kNullDatum, "1"});
 
     // WHEN
     const auto result{RunGreatest().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_least_int_data_mixed_nulls) {
@@ -270,23 +227,13 @@ TEST_F(CelonisGreatestLeastTest, celonis_least_int_data_mixed_nulls) {
     AddRow({kNullDatum, kNullDatum, kNullDatum});
     AddRow({1L, 1L, 1L});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({1L, 1L, 1L, 2L, kNullDatum, 1L})};
+    const auto expected = make_expected<T>({1L, 1L, 1L, 2L, kNullDatum, 1L});
 
     // WHEN
     const auto result{RunLeast().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_least_double_data_mixed_nulls) {
@@ -294,30 +241,20 @@ TEST_F(CelonisGreatestLeastTest, celonis_least_double_data_mixed_nulls) {
     using T = double;
     Prepare(TYPE_DOUBLE, /* number_of_columns */ 3);
 
-    AddRow({1., 2., 3.});
-    AddRow({kNullDatum, 1., 2.});
-    AddRow({2., kNullDatum, 1.});
-    AddRow({3., 2., kNullDatum});
+    AddRow({1.5, 2.0, 3.0});
+    AddRow({kNullDatum, 1.2, 2.0});
+    AddRow({2.0, kNullDatum, 1.0});
+    AddRow({3.0, 2.0, kNullDatum});
     AddRow({kNullDatum, kNullDatum, kNullDatum});
-    AddRow({1., 1., 1.});
+    AddRow({1.0, 1.0, 1.0});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({1., 1., 1., 2., kNullDatum, 1.})};
+    const auto expected = make_expected<T>({1.5, 1.2, 1.0, 2.0, kNullDatum, 1.0});
 
     // WHEN
     const auto result{RunLeast().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_least_datetime_data_mixed_nulls) {
@@ -332,23 +269,13 @@ TEST_F(CelonisGreatestLeastTest, celonis_least_datetime_data_mixed_nulls) {
     AddRow({kNullDatum, kNullDatum, kNullDatum});
     AddRow({1_ts, 1_ts, 1_ts});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({1_ts, 1_ts, 1_ts, 2_ts, kNullDatum, 1_ts})};
+    const auto expected = make_expected<T>({1_ts, 1_ts, 1_ts, 2_ts, kNullDatum, 1_ts});
 
     // WHEN
     const auto result{RunLeast().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 TEST_F(CelonisGreatestLeastTest, celonis_least_varchar_data_mixed_nulls) {
@@ -363,23 +290,13 @@ TEST_F(CelonisGreatestLeastTest, celonis_least_varchar_data_mixed_nulls) {
     AddRow({kNullDatum, kNullDatum, kNullDatum});
     AddRow({"1", "1", "1"});
 
-    const auto [expected_size, expected_nulls, expected_values]{make_expected<T>({"1", "1", "1", "2", kNullDatum, "1"})};
+    const auto expected = make_expected<T>({"1", "1", "1", "2", kNullDatum, "1"});
 
     // WHEN
     const auto result{RunLeast().value()};
 
     // THEN
-    EXPECT_EQ(expected_size, result->size());
-    for (std::size_t row_idx{0}; row_idx < expected_size; ++row_idx) {
-        const bool expected_is_null{expected_nulls.at(row_idx)};
-        const bool actual_is_null{result->get(row_idx).is_null()};
-        EXPECT_EQ(expected_is_null, actual_is_null);
-        if (!actual_is_null) {
-            const auto& expected_value{expected_values.at(row_idx)};
-            const auto actual_value{result->get(row_idx).get<T>()};
-            EXPECT_EQ(expected_value, actual_value);
-        }
-    }
+    validate(result, expected);
 }
 
 } // namespace starrocks
