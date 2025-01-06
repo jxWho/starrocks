a592e53662c8f6c2df5189109f80503f193f05b5
Yaqi Zhang <y.zhang@celonis.com>
2025-01-06 13:17:40 -0600

    [Doc] Update Docstring of CELONIS_XX_HASH3_128 and CELONIS_XX_HASH3_128_NULLABLE (#1431)
    This can make it easier to check what these functions do.
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [x] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/exprs/celonis/string_functions.h
+++ be/src/exprs/celonis/string_functions.h
@@ -8,16 +8,24 @@ namespace starrocks {
 class CelonisStringFunctions {
 public:
     /**
-     * @param: [string_value, ...]
-     * @paramType: [BinaryColumn, ...]
-     * @return LargeIntColumn
+     * @param: [string_value, ...] or [string_array]
+     * @paramType: [VARCHAR, ...] or [ARRAY_VARCHAR]
+     * @return LARGEINT
+     * celonis_xx_hash3_128(NULL) != NULL
+     * celonis_xx_hash3_128(NULL) == celonis_xx_hash3_128(NULL)
+     * celonis_xx_hash3_128(NULL) != celonis_xx_hash3_128(NULL, NULL)
+     * celonis_xx_hash3_128(NULL array) != celonis_xx_hash3_128([NULL]) (NULL array means the type is array and the array is NULL)
+     * celonis_xx_hash3_128([NULL, NULL]) != celonis_xx_hash3_128([NULL])
      */
     DEFINE_VECTORIZED_FN(xx_hash3_128);
 
     /**
-     * @param: [string_value, ...]
-     * @paramType: [BinaryColumn, ...]
-     * @return LargeIntColumn
+     * @param: [string_value, ...] or [string_array]
+     * @paramType: [VARCHAR, ...] or [ARRAY_VARCHAR]
+     * @return LARGEINT
+     * celonis_xx_hash3_128_nullable(NULL, ...) == NULL
+     * celonis_xx_hash3_128_nullable([NULL, ...]) == NULL
+     * celonis_xx_hash3_128_nullable(NULL array) == NULL (NULL array means the type is array and the array is NULL)
      */
     DEFINE_VECTORIZED_FN(xx_hash3_128_nullable);
 
