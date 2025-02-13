d949b867039e82ebcb8b2ec5d9cd6a3a4d429326
Yaqi Zhang <y.zhang@celonis.com>
2025-02-12 19:29:50 -0600

    [Refactor] CELONIS_ARRAY_TRIMMED_MEAN (#1512)
    This PR refactors the implementation of CELONIS_ARRAY_TRIMMED_MEAN and
    adds the support for NULL lower/upper_cutoff.
    
    It adds some unit tests and reformat the test file.
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [x] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [x] Yes, this PR will result in a change in behavior.
    - [ ] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [x] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/exprs/celonis/array_trimmed_mean.cpp
+++ be/src/exprs/celonis/array_trimmed_mean.cpp
@@ -10,45 +10,42 @@
 
 
 namespace starrocks {
-template <LogicalType LT>
+template<LogicalType LT>
 StatusOr<ColumnPtr> CelonisArrayTrimmedMean<LT>::celonis_array_trimmed_mean(FunctionContext* ctx,
                                                                             const Columns& columns) {
-    RETURN_IF_COLUMNS_ONLY_NULL({columns[0]});
-    ColumnHelper::unpack_and_duplicate_const_column(columns[0]->size(), columns[0]);
-    ColumnHelper::unpack_and_duplicate_const_column(columns[0]->size(), columns[1]);
-    ColumnHelper::unpack_and_duplicate_const_column(columns[0]->size(), columns[2]);
+    using CppType = RunTimeCppType<LT>;
+    DCHECK_EQ(3, columns.size());
+    RETURN_IF_COLUMNS_ONLY_NULL({ columns[0] });
+    auto [all_const, n_rows] = ColumnHelper::num_packed_rows(columns);
 
-    const auto [elements, offsets, _, null_elements]{prepare_array_input(columns[0].get())};
+    ColumnPtr array_column = ColumnHelper::unpack_and_duplicate_const_column(n_rows, columns[0]);
+    UnnestedArrayData array_data = prepare_array_input(array_column.get());
+    const auto& elements = down_cast<const RunTimeColumnType<LT>&>(*array_data.elements).get_data().data();
+    const auto& offsets = array_data.offsets->get_data().data();
+    const auto& null_elements = array_data.null_elements;
 
     ColumnViewer lower_cutoff_viewer = ColumnViewer<TYPE_BIGINT>(columns[1]);
     ColumnViewer upper_cutoff_viewer = ColumnViewer<TYPE_BIGINT>(columns[2]);
 
-    const int32_t row_count{static_cast<int32_t>(offsets->size() - 1)};
-    ColumnBuilder<TYPE_DOUBLE> result_column{row_count};
-
-    const auto& offset_data{offsets->get_data()};
-
-    for (int32_t i{0}; i < row_count; i++) {
+    ColumnBuilder<TYPE_DOUBLE> result_column{static_cast<int32_t>(n_rows)};
+    for (auto i{0}; i < n_rows; i++) {
+        if (columns[0]->is_null(i) || lower_cutoff_viewer.is_null(i) || upper_cutoff_viewer.is_null(i)) {
+            result_column.append_null();
+            continue;
+        }
         int64_t lower_cutoff = lower_cutoff_viewer.value(i);
-
         int64_t upper_cutoff = upper_cutoff_viewer.value(i);
         if (lower_cutoff < 0 || lower_cutoff > 100 || upper_cutoff < 0 || upper_cutoff > 100) {
             result_column.append(0.0);
 
         }
-        if (columns[0]->is_null(i)) {
-            result_column.append_null();
-            continue;
-        }
-        const size_t array_size{offset_data[i + 1] - offset_data[i]};
-        using CppType = RunTimeCppType<LT>;
+        const auto start = offsets[i];
+        const auto end = offsets[i + 1];
         std::vector<CppType> new_vector{};
         size_t skipped_values{0};
-        for (size_t group_index{0}; group_index < array_size; group_index++) {
-            if (null_elements == nullptr ||
-                !null_elements->data()[offset_data[i] + group_index]) {
-                new_vector.push_back(elements->get(
-                        offset_data[i] + group_index).get<CppType>());
+        for (auto j = start; j < end; ++j) {
+            if (null_elements == nullptr || (*null_elements)[j] == 0) {
+                new_vector.push_back(elements[j]);
             } else {
                 skipped_values++;
             }
@@ -71,7 +68,7 @@ StatusOr<ColumnPtr> CelonisArrayTrimmedMean<LT>::celonis_array_trimmed_mean(Func
 
         result_column.append(static_cast<double>(sum) / count);
     }
-    return result_column.build(false);
+    return result_column.build(all_const);
 }
 
 template
--- be/src/exprs/celonis/array_trimmed_mean.h
+++ be/src/exprs/celonis/array_trimmed_mean.h
@@ -10,9 +10,11 @@ namespace starrocks {
           * @param: [input_array, lower cutoff, upper cutoff]
           * @paramType [ARRAY_INT | ARRAY_BIGINT | ARRAY_DOUBLE, BIGINT, BIGINT]
           * @return: DOUBLE
-          * This method removes the cutoff of each group and calculates the mean of the remaining values.
-          * Null values are ignored. If a group contains only Null then Null is returned for that group.
-          * If a group is empty, 0 is returned for this group.
+          * This function removes the cutoff of the input array and calculates the mean of the remaining values.
+          * NULL values are ignored. If an array contains only NULL then return NULL.
+          * If an array is empty, 0 is returned for this array.
+          * If the array is NULL, return NULL.
+          * If lower/upper_cutoff is NULL, return NULL.
           * For more information check docs https://docs.celonis.com/en/trimmed_mean.html and https://docs.celonis.com/en/pu_trimmed_mean.html
          */
         DEFINE_VECTORIZED_FN(celonis_array_trimmed_mean);
