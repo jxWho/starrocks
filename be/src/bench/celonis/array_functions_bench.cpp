d565c3705bd6eb63c29fafab67dd15f2388777e1
Yaqi Zhang <y.zhang@celonis.com>
2025-03-14 19:56:41 -0500

    [BugFix] Fix a bug in benchmark of CELONIS_ARRAY_BOOL_OR and CELONIS_ARRAY_COUNT (#1559)
    ## Why I'm doing:
    
    This below line is buggy. Basically for any non-zero state.range(1), the
    probability is set to 1.
    
    bool probability = state.range(1) / 100.0;
    
    ## What I'm doing:
    This PR fixes this bug and updates the benchmark.
    
    ## What type of PR is this:
    
    - [x] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/bench/celonis/array_functions_bench.cpp
+++ be/src/bench/celonis/array_functions_bench.cpp
@@ -13,49 +13,49 @@
 namespace starrocks {
 
 /*
-2025-03-11T18:46:43+00:00
+2025-03-14T15:37:43+00:00
 Running ./be/build_Release/src/bench/celonis/output/array_functions_bench
-Run on (32 X 2998.09 MHz CPU s)
+Run on (32 X 2445.43 MHz CPU s)
 CPU Caches:
   L1 Data 32 KiB (x16)
   L1 Instruction 32 KiB (x16)
   L2 Unified 512 KiB (x16)
   L3 Unified 32768 KiB (x2)
-Load Average: 0.35, 0.62, 1.30
+Load Average: 2.74, 3.94, 3.23
 ----------------------------------------------------------------------------------------------
 Benchmark                                    Time             CPU   Iterations UserCounters...
 ----------------------------------------------------------------------------------------------
 // Args: Number of rows / Number of unique elements in the array / Max number of duplicates per unique element
-BM_DedupSortedByVARCHAR/10000/10/5     2420441 ns      2420222 ns          295 RowInvRate=242.022ns
-BM_DedupSortedByVARCHAR/10000/20/5     6253723 ns      6253078 ns          112 RowInvRate=625.308ns
-BM_DedupSortedByVARCHAR/10000/10/10    5730804 ns      5730227 ns          163 RowInvRate=573.023ns
-BM_DedupSortedByVARCHAR/10000/20/10    9325136 ns      9324398 ns           75 RowInvRate=932.44ns
+BM_DedupSortedByVARCHAR/10000/10/5     2428011 ns      2427770 ns          291 RowInvRate=242.777ns
+BM_DedupSortedByVARCHAR/10000/20/5     6283689 ns      6282785 ns          111 RowInvRate=628.279ns
+BM_DedupSortedByVARCHAR/10000/10/10    4334589 ns      4334238 ns          161 RowInvRate=433.424ns
+BM_DedupSortedByVARCHAR/10000/20/10    9249412 ns      9247639 ns           76 RowInvRate=924.764ns
 // Args: Number of rows / True percentage / Array length
-BM_ArrayBoolOr/1000/1/10                  9381 ns         9333 ns        74790 RowInvRate=9.33322ns
-BM_ArrayBoolOr/10000/1/10                82625 ns        82539 ns         8425 RowInvRate=8.25394ns
-BM_ArrayBoolOr/1000/10/10                 9382 ns         9334 ns        75406 RowInvRate=9.33426ns
-BM_ArrayBoolOr/10000/10/10               82656 ns        82570 ns         8478 RowInvRate=8.257ns
-BM_ArrayBoolOr/1000/50/10                 9376 ns         9332 ns        74876 RowInvRate=9.33185ns
-BM_ArrayBoolOr/10000/50/10               82567 ns        82495 ns         8511 RowInvRate=8.24948ns
-BM_ArrayBoolOr/1000/1/20                  9388 ns         9347 ns        74311 RowInvRate=9.34745ns
-BM_ArrayBoolOr/10000/1/20                82787 ns        82727 ns         8505 RowInvRate=8.27271ns
-BM_ArrayBoolOr/1000/10/20                 9521 ns         9473 ns        74098 RowInvRate=9.47344ns
-BM_ArrayBoolOr/10000/10/20               82814 ns        82744 ns         8475 RowInvRate=8.27439ns
-BM_ArrayBoolOr/1000/50/20                 9396 ns         9351 ns        74846 RowInvRate=9.35054ns
-BM_ArrayBoolOr/10000/50/20               82867 ns        82795 ns         8461 RowInvRate=8.27952ns
+BM_ArrayBoolOr/1000/1/10                 14633 ns        14579 ns        47432 RowInvRate=14.5794ns
+BM_ArrayBoolOr/10000/1/10               139060 ns       138951 ns         5257 RowInvRate=13.8951ns
+BM_ArrayBoolOr/1000/10/10                19303 ns        19249 ns        36953 RowInvRate=19.2493ns
+BM_ArrayBoolOr/10000/10/10              190497 ns       187573 ns         3789 RowInvRate=18.7573ns
+BM_ArrayBoolOr/1000/50/10                20257 ns        20180 ns        32987 RowInvRate=20.1795ns
+BM_ArrayBoolOr/10000/50/10              192363 ns       192259 ns         3855 RowInvRate=19.2259ns
+BM_ArrayBoolOr/1000/1/20                 18957 ns        18899 ns        37428 RowInvRate=18.8988ns
+BM_ArrayBoolOr/10000/1/20               184758 ns       184632 ns         3993 RowInvRate=18.4632ns
+BM_ArrayBoolOr/1000/10/20                21940 ns        21881 ns        32065 RowInvRate=21.8807ns
+BM_ArrayBoolOr/10000/10/20              206817 ns       206707 ns         3340 RowInvRate=20.6707ns
+BM_ArrayBoolOr/1000/50/20                20661 ns        20605 ns        35024 RowInvRate=20.6053ns
+BM_ArrayBoolOr/10000/50/20              182548 ns       182440 ns         3713 RowInvRate=18.244ns
 // Args: Number of rows / Null percentage / Array length
-BM_ArrayCountVarchar/1000/0/20            7788 ns         7678 ns        91441 RowInvRate=7.67766ns
-BM_ArrayCountVarchar/10000/0/20          55052 ns        54935 ns        12709 RowInvRate=5.49348ns
-BM_ArrayCountVarchar/1000/10/20          11394 ns        11343 ns        61868 RowInvRate=11.3434ns
-BM_ArrayCountVarchar/10000/10/20         95358 ns        95275 ns         7344 RowInvRate=9.52754ns
-BM_ArrayCountVarchar/1000/50/20          11262 ns        11225 ns        62460 RowInvRate=11.2255ns
-BM_ArrayCountVarchar/10000/50/20         95092 ns        95030 ns         7346 RowInvRate=9.503ns
-BM_ArrayCountVarchar/1000/0/100           8025 ns         7901 ns        89090 RowInvRate=7.90054ns
-BM_ArrayCountVarchar/10000/0/100         93421 ns        93283 ns         7501 RowInvRate=9.32834ns
-BM_ArrayCountVarchar/1000/10/100         28904 ns        28810 ns        24276 RowInvRate=28.8104ns
-BM_ArrayCountVarchar/10000/10/100       269685 ns       269603 ns         2600 RowInvRate=26.9603ns
-BM_ArrayCountVarchar/1000/50/100         29059 ns        28944 ns        24207 RowInvRate=28.9442ns
-BM_ArrayCountVarchar/10000/50/100       270219 ns       270108 ns         2583 RowInvRate=27.0108ns
+BM_ArrayCountVarchar/1000/0/20           10837 ns        10733 ns        65169 RowInvRate=10.7332ns
+BM_ArrayCountVarchar/10000/0/20          85396 ns        85252 ns        12728 RowInvRate=8.52516ns
+BM_ArrayCountVarchar/1000/10/20          12406 ns        12295 ns        53709 RowInvRate=12.295ns
+BM_ArrayCountVarchar/10000/10/20        106069 ns       105943 ns         6698 RowInvRate=10.5943ns
+BM_ArrayCountVarchar/1000/50/20          13110 ns        13011 ns        56816 RowInvRate=13.0115ns
+BM_ArrayCountVarchar/10000/50/20        106957 ns       106796 ns         7112 RowInvRate=10.6796ns
+BM_ArrayCountVarchar/1000/0/100           8145 ns         8004 ns        63514 RowInvRate=8.00351ns
+BM_ArrayCountVarchar/10000/0/100        126271 ns       125878 ns         5565 RowInvRate=12.5878ns
+BM_ArrayCountVarchar/1000/10/100         30326 ns        30179 ns        21304 RowInvRate=30.1792ns
+BM_ArrayCountVarchar/10000/10/100       335791 ns       335445 ns         2097 RowInvRate=33.5445ns
+BM_ArrayCountVarchar/1000/50/100         30525 ns        30333 ns        21192 RowInvRate=30.3333ns
+BM_ArrayCountVarchar/10000/50/100       306140 ns       305998 ns         2288 RowInvRate=30.5998ns
 */
 
 static void BM_DedupSortedByVARCHAR(benchmark::State& state) {
@@ -117,7 +117,7 @@ static void BM_DedupSortedByVARCHAR(benchmark::State& state) {
 
 static void BM_ArrayBoolOr(benchmark::State& state) {
     int num_rows = state.range(0);
-    bool probability = state.range(1) / 100.0;
+    double probability = state.range(1) / 100.0;
     int array_length = state.range(2);
 
     std::vector<FunctionContext::TypeDesc> arg_types = {
@@ -155,7 +155,7 @@ static void BM_ArrayBoolOr(benchmark::State& state) {
 
 static void BM_ArrayCountVarchar(benchmark::State& state) {
     int num_rows = state.range(0);
-    bool null_probability = state.range(1) / 100.0;
+    double null_probability = state.range(1) / 100.0;
     int array_length = state.range(2);
 
     std::vector<FunctionContext::TypeDesc> arg_types = {
