2833682bea18c82954676bcaf84637a327928eed
Yaqi Zhang <y.zhang@celonis.com>
2025-04-01 05:54:16 -0500

    [Benchmark] Add benchmark of CELONIS_PATINDEX (#1590)
    ## Why I'm doing:
    
    CELONIS_PATINDEX is used in some slow SQLs. It is better to understand
    its performance.
    
    ## What I'm doing:
    
    This PR adds benchmark of CELONIS_PATINDEX. Its performance is very good
    :)
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

---
+++ be/src/bench/celonis/patindex_bench.cpp
@@ -0,0 +1,117 @@
+#include <benchmark/benchmark.h>
+#include <gtest/gtest.h>
+
+#include <random>
+
+#include "column/column_helper.h"
+#include "exprs/anyval_util.h"
+#include "exprs/celonis/patindex.h"
+#include "exprs/function_context.h"
+#include "runtime/types.h"
+#include "testutil/assert.h"
+
+namespace starrocks {
+
+/*
+2025-03-30T17:27:10+00:00
+Running ./be/build_Release/src/bench/celonis/output/patindex_bench
+Run on (32 X 3242.08 MHz CPU s)
+CPU Caches:
+  L1 Data 32 KiB (x16)
+  L1 Instruction 32 KiB (x16)
+  L2 Unified 512 KiB (x16)
+  L3 Unified 32768 KiB (x2)
+Load Average: 5.04, 3.22, 2.28
+----------------------------------------------------------------------------------
+Benchmark                        Time             CPU   Iterations UserCounters...
+----------------------------------------------------------------------------------
+BM_Patindex/1000/20/1        54604 ns        54591 ns        12858 RowInvRate=54.5906ns
+BM_Patindex/10000/20/1      531350 ns       531307 ns         1304 RowInvRate=53.1307ns
+BM_Patindex/100000/20/1    5233597 ns      5233587 ns          133 RowInvRate=52.3359ns
+BM_Patindex/1000/40/1        57197 ns        57175 ns        12264 RowInvRate=57.1749ns
+BM_Patindex/10000/40/1      560195 ns       560193 ns         1234 RowInvRate=56.0193ns
+BM_Patindex/100000/40/1    5561512 ns      5561430 ns          125 RowInvRate=55.6143ns
+BM_Patindex/1000/20/2        53999 ns        53987 ns        13050 RowInvRate=53.9869ns
+BM_Patindex/10000/20/2      531035 ns       531027 ns         1320 RowInvRate=53.1027ns
+BM_Patindex/100000/20/2    5259081 ns      5258995 ns          133 RowInvRate=52.59ns
+BM_Patindex/1000/40/2        57279 ns        57265 ns        12238 RowInvRate=57.2651ns
+BM_Patindex/10000/40/2      564424 ns       564407 ns         1244 RowInvRate=56.4407ns
+BM_Patindex/100000/40/2    5884989 ns      5884508 ns          125 RowInvRate=58.8451ns
+*/
+
+std::string gen_random_str(int min_length, int max_length) {
+    DCHECK_GE(max_length, min_length);
+    if (min_length == 0) {
+        return "";
+    }
+    static std::string alphanum =
+            "0123456789"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz";
+
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> length_dist(min_length, max_length);
+    std::uniform_int_distribution<int> char_dist(0, alphanum.size() - 1);
+
+    int str_len = length_dist(gen);
+    std::string result;
+    result.reserve(str_len);
+    for (int i = 0; i < str_len; i++) {
+        result += alphanum[char_dist(gen)];
+    }
+    return result;
+}
+
+static void do_bench(benchmark::State& state, const std::string& pattern) {
+    int num_rows = state.range(0);
+    int str_length = state.range(1);
+    int64_t index = state.range(2);
+
+    std::vector<FunctionContext::TypeDesc> arg_types = {
+            AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(TYPE_VARCHAR)),
+            AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(TYPE_VARCHAR)),
+            AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(TYPE_BIGINT))};
+    auto return_type = AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(TYPE_BIGINT));
+    std::unique_ptr<FunctionContext> ctx(FunctionContext::create_test_context(std::move(arg_types), return_type));
+
+    int total_rows = 0;
+    for (auto _: state) {
+        state.PauseTiming();
+        total_rows += num_rows;
+        ColumnPtr input_column = ColumnHelper::create_column(TypeDescriptor(TYPE_VARCHAR), true);
+        ColumnPtr pattern_column = ColumnHelper::create_column(TypeDescriptor(TYPE_VARCHAR), true);
+        ColumnPtr index_column = ColumnHelper::create_column(TypeDescriptor(TYPE_BIGINT), true);
+        pattern_column->append_datum(pattern.data());
+        index_column->append_datum(index);
+        pattern_column = ConstColumn::create(pattern_column, num_rows);
+        index_column = ConstColumn::create(index_column, num_rows);
+        for (int i = 0; i < num_rows; i++) {
+            input_column->append_datum(gen_random_str(1, 2 * str_length).data());
+        }
+        ctx->set_constant_columns({nullptr, pattern_column, index_column});
+        Columns columns;
+        columns.push_back(input_column);
+        columns.push_back(pattern_column);
+        columns.push_back(index_column);
+        state.ResumeTiming();
+        ASSERT_OK(CelonisPatindex::prepare(ctx.get(), FunctionContext::FunctionStateScope::FRAGMENT_LOCAL));
+        ASSERT_OK(CelonisPatindex::prepare(ctx.get(), FunctionContext::FunctionStateScope::THREAD_LOCAL));
+        EXPECT_TRUE(CelonisPatindex::patindex(ctx.get(), columns).ok());
+        ASSERT_OK(CelonisPatindex::close(ctx.get(), FunctionContext::FunctionStateScope::FRAGMENT_LOCAL));
+        ASSERT_OK(CelonisPatindex::close(ctx.get(), FunctionContext::FunctionStateScope::THREAD_LOCAL));
+    }
+    state.counters["RowInvRate"] =
+            benchmark::Counter(total_rows, benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+}
+
+static void BM_Patindex(benchmark::State& state) {
+    do_bench(state, "0%");
+}
+
+// Args: Number of rows / String length / Index
+BENCHMARK(BM_Patindex)->ArgsProduct({{1000, 10000, 100000}, {20, 40}, {1, 2}});
+
+} // namespace starrocks
+
+BENCHMARK_MAIN();
