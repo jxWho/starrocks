876c11f4c2e7966fd85f9dcb832a00d6f16235c3
Yaqi Zhang <y.zhang@celonis.com>
2025-04-07 11:01:29 -0500

    [Benchmark] Extend benchmark of CELONIS_XX_HASH3_128_V3 to cover the case with leading constant columns (#1604)
    CELONIS_XX_HASH3_128_V3 is widely used in CREATE_EVENTLOG with
    enableJoinIndex = True. And many of the use case has a leading constant
    column (i.e., unique_event_id = CELONIS_XX_HASH3_128_V3(EVENT_TABLE,
    EVENT_ID)). We need to understand its behavior.
    
    This PR extends benchmark of CELONIS_XX_HASH3_128_V3 to cover the case
    with leading constant columns.
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/bench/celonis/xx_hash3_128_v3_bench.cpp
+++ be/src/bench/celonis/xx_hash3_128_v3_bench.cpp
@@ -13,28 +13,37 @@
 namespace starrocks {
 
 /*
-2025-03-10T19:13:00+00:00
+2025-04-05T01:10:31+00:00
 Running ./be/build_Release/src/bench/celonis/output/xx_hash3_128_v3_bench
-Run on (32 X 3105.45 MHz CPU s)
+Run on (32 X 3241.5 MHz CPU s)
 CPU Caches:
   L1 Data 32 KiB (x16)
   L1 Instruction 32 KiB (x16)
   L2 Unified 512 KiB (x16)
   L3 Unified 32768 KiB (x2)
-Load Average: 0.28, 1.76, 9.44
-// Args: Number of rows // Number of input columns / Average length of string
------------------------------------------------------------------------------------
-Benchmark                         Time             CPU   Iterations UserCounters...
------------------------------------------------------------------------------------
-BM_Hash128V3/100000/1/10   11906416 ns     11906654 ns           60 RowInvRate=119.067ns
-BM_Hash128V3/100000/2/10   16754586 ns     16751585 ns           42 RowInvRate=167.516ns
-BM_Hash128V3/100000/3/10   20996633 ns     20992097 ns           33 RowInvRate=209.921ns
-BM_Hash128V3/100000/1/20   11399903 ns     11399384 ns           61 RowInvRate=113.994ns
-BM_Hash128V3/100000/2/20   17770555 ns     17770182 ns           39 RowInvRate=177.702ns
-BM_Hash128V3/100000/3/20   28975125 ns     28973272 ns           24 RowInvRate=289.733ns
-BM_Hash128V3/100000/1/40   11527241 ns     11527025 ns           61 RowInvRate=115.27ns
-BM_Hash128V3/100000/2/40   25774769 ns     25774113 ns           27 RowInvRate=257.741ns
-BM_Hash128V3/100000/3/40   36962701 ns     36962418 ns           19 RowInvRate=369.624ns
+Load Average: 7.09, 7.05, 5.13
+// Args: Number of rows / Number of input columns / Number of leading constant columns / Average length of string
+-------------------------------------------------------------------------------------
+Benchmark                           Time             CPU   Iterations UserCounters...
+-------------------------------------------------------------------------------------
+BM_Hash128V3/100000/1/0/10   11736158 ns     11734488 ns           62 RowInvRate=117.345ns
+BM_Hash128V3/100000/2/0/10   19823743 ns     19823487 ns           37 RowInvRate=198.235ns
+BM_Hash128V3/100000/3/0/10   21857086 ns     21856227 ns           31 RowInvRate=218.562ns
+BM_Hash128V3/100000/1/1/10   10896280 ns     10897017 ns           64 RowInvRate=108.97ns
+BM_Hash128V3/100000/2/1/10   15046540 ns     15046172 ns           48 RowInvRate=150.462ns
+BM_Hash128V3/100000/3/1/10   19433124 ns     19433226 ns           36 RowInvRate=194.332ns
+BM_Hash128V3/100000/1/0/20   11920554 ns     11920901 ns           59 RowInvRate=119.209ns
+BM_Hash128V3/100000/2/0/20   17715096 ns     17714928 ns           40 RowInvRate=177.149ns
+BM_Hash128V3/100000/3/0/20   33940065 ns     33938675 ns           21 RowInvRate=339.387ns
+BM_Hash128V3/100000/1/1/20   11436854 ns     11437395 ns           64 RowInvRate=114.374ns
+BM_Hash128V3/100000/2/1/20   14989199 ns     14970764 ns           46 RowInvRate=149.708ns
+BM_Hash128V3/100000/3/1/20   25723661 ns     25723762 ns           26 RowInvRate=257.238ns
+BM_Hash128V3/100000/1/0/40   11614352 ns     11613671 ns           61 RowInvRate=116.137ns
+BM_Hash128V3/100000/2/0/40   28559150 ns     28559129 ns           28 RowInvRate=285.591ns
+BM_Hash128V3/100000/3/0/40   36507556 ns     36508175 ns           18 RowInvRate=365.082ns
+BM_Hash128V3/100000/1/1/40   14081614 ns     14080683 ns           64 RowInvRate=140.807ns
+BM_Hash128V3/100000/2/1/40   30114178 ns     30110763 ns           25 RowInvRate=301.108ns
+BM_Hash128V3/100000/3/1/40   36557612 ns     36553197 ns           17 RowInvRate=365.532ns
 */
 
 std::string generate_random_string(int min_len, int max_len) {
@@ -62,7 +71,9 @@ std::string generate_random_string(int min_len, int max_len) {
 static void do_bench(benchmark::State& state) {
     int num_rows = state.range(0);
     int num_cols = state.range(1);
-    int avg_length = state.range(2);
+    int num_const_cols = state.range(2);
+    int avg_length = state.range(3);
+    DCHECK_GE(num_cols, num_const_cols);
 
     std::vector<FunctionContext::TypeDesc> arg_types;
     arg_types.reserve(num_cols);
@@ -83,12 +94,22 @@ static void do_bench(benchmark::State& state) {
         total_rows += num_rows;
         for (auto col = 0; col < num_cols; ++col) {
             auto input_column = ColumnHelper::create_column(TypeDescriptor(TYPE_VARCHAR), true);
-            for (int i = 0; i < num_rows; i++) {
-                input_column->append_datum(Slice(generate_random_string(min_length, max_length)));
+            if (col < num_const_cols) {
+                input_column->append_datum(Slice(generate_random_string(avg_length, avg_length)));
+                input_column = ConstColumn::create(input_column, num_rows);
+            } else {
+                for (int i = 0; i < num_rows; i++) {
+                    input_column->append_datum(Slice(generate_random_string(min_length, max_length)));
+                }
             }
             input_columns.push_back(input_column);
         }
-       ctx->set_constant_columns({nullptr});
+        starrocks::Columns constant_columns;
+        constant_columns.reserve(num_cols);
+        for (const auto& input_column : input_columns) {
+            constant_columns.push_back(input_column->is_constant() ? input_column : nullptr);
+        }
+        ctx->set_constant_columns(constant_columns);
 
         state.ResumeTiming();
         ASSERT_TRUE(CelonisStringFunctions::xx_hash3_128_v3(ctx.get(), input_columns).ok());
@@ -101,8 +122,8 @@ static void BM_Hash128V3(benchmark::State& state) {
     do_bench(state);
 }
 
-// Args: Number of rows // Number of input columns / Average length of string
-BENCHMARK(BM_Hash128V3)->ArgsProduct({{100000}, {1, 2, 3}, {10, 20, 40}});
+// Args: Number of rows / Number of input columns / Number of leading constant columns / Average length of string
+BENCHMARK(BM_Hash128V3)->ArgsProduct({{100000}, {1, 2, 3}, {0, 1}, {10, 20, 40}});
 
 } // namespace starrocks
 
