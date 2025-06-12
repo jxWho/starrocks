ee41bb4ff034f8f027dedd883e833a9451b34bc3
Yaqi Zhang <y.zhang@celonis.com>
2025-06-12 12:48:26 -0500

    [Benchmark] Update benchmark of CELONIS_ARRAY_COUNT_DISTINCT (#1725)
    ## Why I'm doing:
    
    The original benchmark of CELONIS_ARRAY_COUNT_DISTINCT consider NULL
    percentage of the array which is not related to the function.
    
    ## What I'm doing:
    
    This PR changes CELONIS_ARRAY_COUNT_DISTINCT benchmark to consider
    duplicate ratio.
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/bench/celonis/array_count_distinct_bench.cpp
+++ be/src/bench/celonis/array_count_distinct_bench.cpp
@@ -13,42 +13,48 @@
 namespace starrocks {
 
 /*
-2025-03-14T18:44:27+00:00
+2025-06-10T19:39:01+00:00
 Running ./be/build_Release/src/bench/celonis/output/array_count_distinct_bench
-Run on (32 X 2445.43 MHz CPU s)
+Run on (32 X 3401.86 MHz CPU s)
 CPU Caches:
   L1 Data 32 KiB (x16)
   L1 Instruction 32 KiB (x16)
   L2 Unified 512 KiB (x16)
   L3 Unified 32768 KiB (x2)
-Load Average: 2.04, 4.21, 3.17
-// Args: Number of rows / Null percentage / Array length
+Load Average: 3.89, 5.48, 9.11
+// Args: Number of rows / Duplicate rate percentage / Array length
 ---------------------------------------------------------------------------------------------------
 Benchmark                                         Time             CPU   Iterations UserCounters...
 ---------------------------------------------------------------------------------------------------
-BM_ArrayCountDistinctVarchar/1000/0/20       306468 ns       306484 ns         2285 RowInvRate=306.484ns
-BM_ArrayCountDistinctVarchar/10000/0/20     3029414 ns      3029271 ns          234 RowInvRate=302.927ns
-BM_ArrayCountDistinctVarchar/1000/10/20      304771 ns       304761 ns         2295 RowInvRate=304.761ns
-BM_ArrayCountDistinctVarchar/10000/10/20    3022134 ns      3022003 ns          232 RowInvRate=302.2ns
-BM_ArrayCountDistinctVarchar/1000/50/20      275750 ns       275698 ns         2297 RowInvRate=275.698ns
-BM_ArrayCountDistinctVarchar/10000/50/20    2606932 ns      2606719 ns          267 RowInvRate=260.672ns
-BM_ArrayCountDistinctVarchar/1000/0/40       577666 ns       577644 ns         1216 RowInvRate=577.644ns
-BM_ArrayCountDistinctVarchar/10000/0/40     5670657 ns      5670353 ns          123 RowInvRate=567.035ns
-BM_ArrayCountDistinctVarchar/1000/10/40      572252 ns       572159 ns         1233 RowInvRate=572.159ns
-BM_ArrayCountDistinctVarchar/10000/10/40    5651476 ns      5651027 ns          124 RowInvRate=565.103ns
-BM_ArrayCountDistinctVarchar/1000/50/40      500755 ns       500755 ns         1000 RowInvRate=500.755ns
-BM_ArrayCountDistinctVarchar/10000/50/40    4890991 ns      4890760 ns          135 RowInvRate=489.076ns
-BM_ArrayCountDistinctVarchar/1000/0/80      1145877 ns      1145818 ns          607 RowInvRate=1.14582us
-BM_ArrayCountDistinctVarchar/10000/0/80    12945317 ns     12944693 ns           56 RowInvRate=1.29447us
-BM_ArrayCountDistinctVarchar/1000/10/80     1284064 ns      1283786 ns          567 RowInvRate=1.28379us
-BM_ArrayCountDistinctVarchar/10000/10/80   13459372 ns     13452945 ns           56 RowInvRate=1.34529us
-BM_ArrayCountDistinctVarchar/1000/50/80     1138478 ns      1138114 ns          535 RowInvRate=1.13811us
-BM_ArrayCountDistinctVarchar/10000/50/80   10783511 ns     10782942 ns           64 RowInvRate=1078.29ns
+BM_ArrayCountDistinctVarchar/1000/0/20       435112 ns       435119 ns         1610 RowInvRate=435.119ns
+BM_ArrayCountDistinctVarchar/10000/0/20     4301370 ns      4301330 ns          162 RowInvRate=430.133ns
+BM_ArrayCountDistinctVarchar/1000/25/20      417451 ns       417446 ns         1681 RowInvRate=417.446ns
+BM_ArrayCountDistinctVarchar/10000/25/20    4105679 ns      4105515 ns          170 RowInvRate=410.552ns
+BM_ArrayCountDistinctVarchar/1000/50/20      351797 ns       351777 ns         1993 RowInvRate=351.777ns
+BM_ArrayCountDistinctVarchar/10000/50/20    3485722 ns      3485672 ns          199 RowInvRate=348.567ns
+BM_ArrayCountDistinctVarchar/1000/75/20      327216 ns       327201 ns         2131 RowInvRate=327.201ns
+BM_ArrayCountDistinctVarchar/10000/75/20    3245173 ns      3245209 ns          216 RowInvRate=324.521ns
+BM_ArrayCountDistinctVarchar/1000/0/40       787955 ns       787938 ns          892 RowInvRate=787.938ns
+BM_ArrayCountDistinctVarchar/10000/0/40     7845502 ns      7845463 ns           88 RowInvRate=784.546ns
+BM_ArrayCountDistinctVarchar/1000/25/40      822796 ns       822784 ns          850 RowInvRate=822.784ns
+BM_ArrayCountDistinctVarchar/10000/25/40    8237105 ns      8236975 ns           86 RowInvRate=823.697ns
+BM_ArrayCountDistinctVarchar/1000/50/40      806044 ns       806020 ns          867 RowInvRate=806.02ns
+BM_ArrayCountDistinctVarchar/10000/50/40    8019104 ns      8018813 ns           86 RowInvRate=801.881ns
+BM_ArrayCountDistinctVarchar/1000/75/40      590799 ns       590748 ns         1184 RowInvRate=590.748ns
+BM_ArrayCountDistinctVarchar/10000/75/40    5838231 ns      5837914 ns          120 RowInvRate=583.791ns
+BM_ArrayCountDistinctVarchar/1000/0/80      1476279 ns      1476242 ns          474 RowInvRate=1.47624us
+BM_ArrayCountDistinctVarchar/10000/0/80    15702665 ns     15702620 ns           45 RowInvRate=1.57026us
+BM_ArrayCountDistinctVarchar/1000/25/80     1520072 ns      1519984 ns          462 RowInvRate=1.51998us
+BM_ArrayCountDistinctVarchar/10000/25/80   16184125 ns     16182771 ns           43 RowInvRate=1.61828us
+BM_ArrayCountDistinctVarchar/1000/50/80     1465881 ns      1465810 ns          478 RowInvRate=1.46581us
+BM_ArrayCountDistinctVarchar/10000/50/80   15620801 ns     15620754 ns           45 RowInvRate=1.56208us
+BM_ArrayCountDistinctVarchar/1000/75/80     1399831 ns      1399779 ns          501 RowInvRate=1.39978us
+BM_ArrayCountDistinctVarchar/10000/75/80   14998936 ns     14998300 ns           47 RowInvRate=1.49983us
 */
 
 static void BM_ArrayCountDistinctVarchar(benchmark::State& state) {
     int num_rows = state.range(0);
-    double null_probability = state.range(1) / 100.0;
+    double duplicate_rate = state.range(1) / 100.0;
     int array_length = state.range(2);
 
     std::vector<FunctionContext::TypeDesc> arg_types = {
@@ -59,13 +65,20 @@ static void BM_ArrayCountDistinctVarchar(benchmark::State& state) {
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::bernoulli_distribution dist(null_probability);
 
-    std::vector<std::string> strings;
-    strings.reserve(array_length);
-    for (int j = 0; j < array_length; ++j) {
-        strings.emplace_back("value" + std::to_string(j));
+    // Calculate the number of unique values based on duplicate rate
+    // If duplicate_rate is 0%, all values are unique (array_length unique values)
+    // If duplicate_rate is 50%, half the values are duplicates (array_length/2 unique values)
+    int num_unique_values = std::max(1, static_cast<int>(array_length * (1.0 - duplicate_rate)));
+
+    std::vector<std::string> unique_strings;
+    unique_strings.reserve(num_unique_values);
+    for (int j = 0; j < num_unique_values; ++j) {
+        unique_strings.emplace_back("value" + std::to_string(j));
     }
+
+    std::uniform_int_distribution<> value_dist(0, num_unique_values - 1);
+
     int total_rows = 0;
     for (auto _: state) {
         state.PauseTiming();
@@ -76,12 +89,8 @@ static void BM_ArrayCountDistinctVarchar(benchmark::State& state) {
             DatumArray input_array;
             input_array.reserve(array_length);
             for (int j = 0; j < array_length; j++) {
-                bool is_null = dist(gen);
-                if (is_null) {
-                    input_array.emplace_back(kNullDatum);
-                } else {
-                    input_array.emplace_back(Slice(strings[j]));
-                }
+                int value_index = value_dist(gen);
+                input_array.emplace_back(Slice(unique_strings[value_index]));
             }
             input_column->append_datum(input_array);
         }
@@ -95,8 +104,8 @@ static void BM_ArrayCountDistinctVarchar(benchmark::State& state) {
             benchmark::Counter(total_rows, benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
 }
 
-// Args: Number of rows / Null percentage / Array length
-BENCHMARK(BM_ArrayCountDistinctVarchar)->ArgsProduct({{1000, 10000}, {0, 10, 50}, {20, 40, 80}});
+// Args: Number of rows / Duplicate rate percentage / Array length
+BENCHMARK(BM_ArrayCountDistinctVarchar)->ArgsProduct({{1000, 10000}, {0, 25, 50, 75}, {20, 40, 80}});
 
 } // namespace starrocks
 
