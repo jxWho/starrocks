621cf8f2478c46646c19dfd6e48aa543dca760f5
Pavan Kalyan <p.damalapati@celonis.com>
2025-07-09 17:19:12 -0400

    Making ddprof log levels configurable via environment variable (#1780)
    ## Why I'm doing:
    
    This is so we have better control over the log level of ddprof.
    Particularly to remove debug level ddprof logs.
    
    
    ## What I'm doing:
    Making ddprof log level configurable via environment variables.
    If I make a change upstream and eventually merge when we have a
    conflict, we can drop everything between
    ```
    ##### celonis start #####
    ....
    ##### celonis end #####
    ```
    
    Tested using this
    [PR](https://github.com/celonis/celostar/pull/13606/files) when I set it
    to "warn", it excluded all debug and informational logs.
    
    Fixes #issue
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [x] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [x] Yes, this PR will result in a change in behavior.
    - [ ] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- bin/start_backend.sh
+++ bin/start_backend.sh
@@ -225,7 +225,18 @@ fi
 
 # enable DD profile
 if [ "${ENABLE_DATADOG_PROFILE}" == "true" ] && [ -f "${STARROCKS_HOME}/datadog/ddprof" ]; then
-    START_BE_CMD="${STARROCKS_HOME}/datadog/ddprof -l debug ${START_BE_CMD}"
+
+    ####### celonis start #######
+
+    ddp_log_level="debug"
+    if [ -n "${DDPROF_LOG_LEVEL}" ]; then
+        dd_log_level="${DDPROF_LOG_LEVEL}"
+    fi
+
+    START_BE_CMD="${STARROCKS_HOME}/datadog/ddprof -l ${dd_log_level} ${START_BE_CMD}"
+
+    ####### celonis end #######
+
 fi
 
 if [ ${RUN_LOG_CONSOLE} -eq 1 ] ; then
