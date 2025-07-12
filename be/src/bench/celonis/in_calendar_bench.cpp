8147dff8fa882c8e5fb9f23e829a3dd03fa22b55
Yaqi Zhang <y.zhang@celonis.com>
2025-07-12 05:47:10 -0500

    [Enhancement] Improve benchmark of CELONIS_IN_CALENDAR (#1788)
    ## Why I'm doing:
    
    We want to understand the performance of CELONIS_IN_CALENDAR better.
    
    ## What I'm doing:
    
    This PR improves the `in_calendar` benchmark to provide more accurate
    performance measurements and adds coverage for `WeekdayCalendar` type.
    
    ### 1. Fixed timing measurement to exclude prepare overhead
    
    Previously, the benchmark included the `in_calendar_prepare` calls
    within the timed section, which added ~2.6ms of setup overhead per
    iteration. This made it impossible to accurately measure the actual
    per-row lookup performance, especially for small batches where setup
    cost dominated (97% of time for 1000 rows).
    
    **Before:** (same FactoryCalendar)
    - 1,000 rows: 2,729ns per row
    - 100,000 rows: 96ns per row
    
    **After:**
    - 1,000 rows: 73ns per row  
    - 100,000 rows: 66ns per row
    
    ### 2. Added WeekdayCalendar benchmark support
    
    Extended the benchmark to test both `FactoryCalendar` and
    `WeekdayCalendar` types:
    - Added a boolean parameter to switch between calendar types
    - Implemented a standard Monday-Friday 9AM-5PM schedule for weekday
    calendars
    - Separate benchmark runs for each calendar type to enable performance
    comparison
    
    Based on the benchmark, the performance of CELONIS_IN_CALENDAR is good.
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [x] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

--- be/src/bench/celonis/in_calendar_bench.cpp
+++ be/src/bench/celonis/in_calendar_bench.cpp
@@ -16,46 +16,57 @@
 namespace starrocks {
 
 /*
-2025-07-09T14:46:39+00:00
+2025-07-11T14:35:28+00:00
 Running ./be/build_Release/src/bench/celonis/output/in_calendar_bench
-Run on (32 X 2943.08 MHz CPU s)
+Run on (32 X 3006.7 MHz CPU s)
 CPU Caches:
   L1 Data 32 KiB (x16)
   L1 Instruction 32 KiB (x16)
   L2 Unified 512 KiB (x16)
   L3 Unified 32768 KiB (x2)
-Load Average: 3.55, 8.08, 5.32
-// Args: Number of rows / Number of calendar entries per id / Number of calendar ids
---------------------------------------------------------------------------------------
-Benchmark                            Time             CPU   Iterations UserCounters...
---------------------------------------------------------------------------------------
-BM_InCalendar/1000/10/2          81328 ns        81363 ns         8558 RowInvRate=81.3631ns
-BM_InCalendar/10000/10/2        708965 ns       708989 ns          984 RowInvRate=70.8989ns
-BM_InCalendar/100000/10/2      6954011 ns      6954044 ns          100 RowInvRate=69.5404ns
-BM_InCalendar/1000/100/2        133634 ns       133709 ns         5234 RowInvRate=133.709ns
-BM_InCalendar/10000/100/2       766601 ns       766615 ns          915 RowInvRate=76.6615ns
-BM_InCalendar/100000/100/2     7040569 ns      7040395 ns           98 RowInvRate=70.4039ns
-BM_InCalendar/1000/1000/2       697408 ns       697567 ns         1003 RowInvRate=697.567ns
-BM_InCalendar/10000/1000/2     1350845 ns      1350918 ns          521 RowInvRate=135.092ns
-BM_InCalendar/100000/1000/2    7649122 ns      7649045 ns           92 RowInvRate=76.4905ns
-BM_InCalendar/1000/10/4          84597 ns        84641 ns         8258 RowInvRate=84.641ns
-BM_InCalendar/10000/10/4        647185 ns       647207 ns         1078 RowInvRate=64.7207ns
-BM_InCalendar/100000/10/4      6225772 ns      6225678 ns          111 RowInvRate=62.2568ns
-BM_InCalendar/1000/100/4        188173 ns       188285 ns         3665 RowInvRate=188.285ns
-BM_InCalendar/10000/100/4       766713 ns       766693 ns          913 RowInvRate=76.6693ns
-BM_InCalendar/100000/100/4     6426668 ns      6426435 ns          108 RowInvRate=64.2643ns
-BM_InCalendar/1000/1000/4      1379122 ns      1379256 ns          506 RowInvRate=1.37926us
-BM_InCalendar/10000/1000/4     1964955 ns      1964904 ns          355 RowInvRate=196.49ns
-BM_InCalendar/100000/1000/4    7670482 ns      7670523 ns           90 RowInvRate=76.7052ns
-BM_InCalendar/1000/10/8         102316 ns       102365 ns         6769 RowInvRate=102.365ns
-BM_InCalendar/10000/10/8        703565 ns       703588 ns          997 RowInvRate=70.3588ns
-BM_InCalendar/100000/10/8      6656882 ns      6656786 ns          105 RowInvRate=66.5679ns
-BM_InCalendar/1000/100/8        322489 ns       322609 ns         2137 RowInvRate=322.609ns
-BM_InCalendar/10000/100/8       933290 ns       933335 ns          747 RowInvRate=93.3335ns
-BM_InCalendar/100000/100/8     6969412 ns      6969216 ns          101 RowInvRate=69.6922ns
-BM_InCalendar/1000/1000/8      2729408 ns      2729564 ns          257 RowInvRate=2.72956us
-BM_InCalendar/10000/1000/8     3363714 ns      3363812 ns          208 RowInvRate=336.381ns
-BM_InCalendar/100000/1000/8    9583175 ns      9583115 ns           74 RowInvRate=95.8312ns
+Load Average: 31.86, 24.05, 13.07
+// Args: Number of rows / Number of calendar entries (ignored for weekday) / Number of calendar ids / Use weekday calendar
+----------------------------------------------------------------------------------------
+Benchmark                              Time             CPU   Iterations UserCounters...
+----------------------------------------------------------------------------------------
+// FactoryCalendar
+BM_InCalendar/1000/10/2/0          64252 ns        64228 ns        10889 RowInvRate=64.2278ns
+BM_InCalendar/10000/10/2/0        623580 ns       623518 ns         1116 RowInvRate=62.3518ns
+BM_InCalendar/100000/10/2/0      6188232 ns      6186779 ns          113 RowInvRate=61.8678ns
+BM_InCalendar/1000/100/2/0         65848 ns        65798 ns        10641 RowInvRate=65.7977ns
+BM_InCalendar/10000/100/2/0       638811 ns       638719 ns         1094 RowInvRate=63.8719ns
+BM_InCalendar/100000/100/2/0     6344859 ns      6344004 ns          109 RowInvRate=63.44ns
+BM_InCalendar/1000/1000/2/0        67392 ns        67184 ns        10412 RowInvRate=67.1842ns
+BM_InCalendar/10000/1000/2/0      641452 ns       641369 ns         1083 RowInvRate=64.1369ns
+BM_InCalendar/100000/1000/2/0    6355974 ns      6355210 ns          110 RowInvRate=63.5521ns
+BM_InCalendar/1000/10/4/0          64374 ns        64339 ns        11031 RowInvRate=64.3391ns
+BM_InCalendar/10000/10/4/0        622387 ns       622363 ns         1123 RowInvRate=62.2363ns
+BM_InCalendar/100000/10/4/0      6182991 ns      6182927 ns          113 RowInvRate=61.8293ns
+BM_InCalendar/1000/100/4/0         64814 ns        64749 ns        10798 RowInvRate=64.7486ns
+BM_InCalendar/10000/100/4/0       627103 ns       627042 ns         1118 RowInvRate=62.7042ns
+BM_InCalendar/100000/100/4/0     6206725 ns      6206574 ns          112 RowInvRate=62.0657ns
+BM_InCalendar/1000/1000/4/0        66528 ns        66276 ns        10534 RowInvRate=66.2762ns
+BM_InCalendar/10000/1000/4/0      633608 ns       633462 ns         1100 RowInvRate=63.3462ns
+BM_InCalendar/100000/1000/4/0    6250123 ns      6250001 ns          111 RowInvRate=62.5ns
+BM_InCalendar/1000/10/8/0          66409 ns        66385 ns        10488 RowInvRate=66.3854ns
+BM_InCalendar/10000/10/8/0        647490 ns       647447 ns         1088 RowInvRate=64.7447ns
+BM_InCalendar/100000/10/8/0      6380250 ns      6379632 ns          110 RowInvRate=63.7963ns
+BM_InCalendar/1000/100/8/0         67649 ns        67506 ns        10358 RowInvRate=67.5064ns
+BM_InCalendar/10000/100/8/0       652854 ns       652769 ns         1075 RowInvRate=65.2769ns
+BM_InCalendar/100000/100/8/0     6510915 ns      6510176 ns          112 RowInvRate=65.1018ns
+BM_InCalendar/1000/1000/8/0        69957 ns        69666 ns        10027 RowInvRate=69.6664ns
+BM_InCalendar/10000/1000/8/0      653867 ns       653655 ns         1070 RowInvRate=65.3655ns
+BM_InCalendar/100000/1000/8/0    6459604 ns      6459369 ns          106 RowInvRate=64.5937ns
+// WeekdayCalendar
+BM_InCalendar/1000/0/2/1          124579 ns       124557 ns         5635 RowInvRate=124.557ns
+BM_InCalendar/10000/0/2/1        1228159 ns      1228102 ns          571 RowInvRate=122.81ns
+BM_InCalendar/100000/0/2/1      12232851 ns     12232276 ns           57 RowInvRate=122.323ns
+BM_InCalendar/1000/0/4/1          123051 ns       123031 ns         5693 RowInvRate=123.031ns
+BM_InCalendar/10000/0/4/1        1215721 ns      1215608 ns          577 RowInvRate=121.561ns
+BM_InCalendar/100000/0/4/1      12121826 ns     12121344 ns           58 RowInvRate=121.213ns
+BM_InCalendar/1000/0/8/1          126552 ns       126531 ns         5566 RowInvRate=126.531ns
+BM_InCalendar/10000/0/8/1        1218557 ns      1218496 ns          572 RowInvRate=121.85ns
+BM_InCalendar/100000/0/8/1      12208510 ns     12207887 ns           57 RowInvRate=122.079ns
 */
 
 static void BM_InCalendar(benchmark::State& state) {
@@ -63,6 +74,7 @@ static void BM_InCalendar(benchmark::State& state) {
     int num_rows = state.range(0);
     int num_calendar_entries = state.range(1);
     int num_calendar_ids = state.range(2);
+    bool use_weekday_calendar = state.range(3);
 
     using UniformInt = std::uniform_int_distribution<int32_t>;
     std::random_device dev;
@@ -80,23 +92,56 @@ static void BM_InCalendar(benchmark::State& state) {
             AnyValUtil::column_type_to_type_desc(TypeDescriptor(TYPE_DATETIME)),
             AnyValUtil::column_type_to_type_desc(TypeDescriptor::create_array_type(TypeDescriptor(TYPE_VARCHAR))),
             AnyValUtil::column_type_to_type_desc(TypeDescriptor(TYPE_VARCHAR))};
-    auto return_type =
-            AnyValUtil::column_type_to_type_desc(TypeDescriptor(TYPE_BIGINT));
+    auto return_type = AnyValUtil::column_type_to_type_desc(TypeDescriptor(TYPE_BIGINT));
     std::unique_ptr<FunctionContext> ctx(FunctionContext::create_test_context(std::move(arg_types), return_type));
 
-    celonis::accelerator::FactoryCalendar factory_calendar;
-    for (auto i = 0; i < num_calendar_ids; ++i) {
-        std::string calendar_id = calendar_ids[i];
-        for (auto j = 0; j < num_calendar_entries; ++j) {
-            celonis::accelerator::FactoryCalendarEntry* entry = factory_calendar.add_entries();
-            int unix_seconds = uniform_timestamp_increase(rng);
-            entry->set_start_date(unix_seconds * 1000);
-            entry->set_end_date((unix_seconds + 3600) * 1000); // the size of the entry is 1 hour.
-            entry->set_calendar_id(calendar_id);
-        }
-    }
     celonis::accelerator::Calendar calendar_proto;
-    *calendar_proto.mutable_factory_calendar() = factory_calendar;
+    if (use_weekday_calendar) {
+        // Create WeekdayCalendar (Monday-Friday 9-5)
+        celonis::accelerator::MultiWeekdayCalendar multi_weekday_calendar;
+        for (auto i = 0; i < num_calendar_ids; ++i) {
+            celonis::accelerator::WeekdayCalendar* weekday_cal = multi_weekday_calendar.add_calendars();
+            weekday_cal->set_calendar_id(calendar_ids[i]);
+            // Helper to create a weekday entry
+            auto create_weekday_entry = [](bool use_day, int begin_hour = 9, int end_hour = 17) {
+                celonis::accelerator::WeekdayCalendarEntry entry;
+                entry.set_use_day(use_day);
+                if (use_day) {
+                    celonis::accelerator::WeekdayCalendarEntryShift* shift = entry.mutable_shift();
+                    shift->set_begin(begin_hour * 3600 * 1000); // Convert hours to milliseconds
+                    shift->set_end(end_hour * 3600 * 1000);
+                }
+                return entry;
+            };
+
+            // Monday-Friday: 9AM-5PM
+            *weekday_cal->mutable_monday() = create_weekday_entry(true);
+            *weekday_cal->mutable_tuesday() = create_weekday_entry(true);
+            *weekday_cal->mutable_wednesday() = create_weekday_entry(true);
+            *weekday_cal->mutable_thursday() = create_weekday_entry(true);
+            *weekday_cal->mutable_friday() = create_weekday_entry(true);
+
+            // Weekend: closed
+            *weekday_cal->mutable_saturday() = create_weekday_entry(false);
+            *weekday_cal->mutable_sunday() = create_weekday_entry(false);
+        }
+
+        *calendar_proto.mutable_multi_weekday_calendar() = multi_weekday_calendar;
+    } else {
+        celonis::accelerator::FactoryCalendar factory_calendar;
+        for (auto i = 0; i < num_calendar_ids; ++i) {
+            std::string calendar_id = calendar_ids[i];
+            for (auto j = 0; j < num_calendar_entries; ++j) {
+                celonis::accelerator::FactoryCalendarEntry* entry = factory_calendar.add_entries();
+                int unix_seconds = uniform_timestamp_increase(rng);
+                entry->set_start_date(unix_seconds * 1000);
+                entry->set_end_date((unix_seconds + 3600) * 1000);
+                entry->set_calendar_id(calendar_id);
+            }
+        }
+        *calendar_proto.mutable_factory_calendar() = factory_calendar;
+    }
+
     std::optional<std::string> serialized_calendar = to_base64_encoded_string(calendar_proto, 1LL << 30, true);
     ASSERT_TRUE(serialized_calendar.has_value());
     DatumArray calendar_array;
@@ -107,42 +152,48 @@ static void BM_InCalendar(benchmark::State& state) {
     for (auto _ : state) {
         state.PauseTiming();
         total_rows += num_rows;
-        auto timestamp_column =
-                ColumnHelper::create_column(TypeDescriptor(TYPE_DATETIME), true);
-        auto calendar_column =
-                ColumnHelper::create_column(TypeDescriptor::create_array_type(TypeDescriptor(TYPE_VARCHAR)), true);
-        auto calendar_id_column =
-                ColumnHelper::create_column(TypeDescriptor(TYPE_VARCHAR), true);
+        auto timestamp_column = ColumnHelper::create_column(TypeDescriptor(TYPE_DATETIME), true);
+        auto calendar_column = ColumnHelper::create_column(TypeDescriptor::create_array_type(TypeDescriptor(TYPE_VARCHAR)), true);
+        auto calendar_id_column = ColumnHelper::create_column(TypeDescriptor(TYPE_VARCHAR), true);
 
         calendar_column->append_datum(calendar_array);
         calendar_column = ConstColumn::create(calendar_column, num_rows);
+
         for (int i = 0; i < num_rows; i++) {
             int unix_seconds = uniform_timestamp_increase(rng);
             timestamp.from_unix_second(unix_seconds);
             timestamp_column->append_datum(timestamp);
             calendar_id_column->append_datum(Slice(calendar_ids[uniform_calendar_id(rng)]));
         }
+
         ctx->set_constant_columns({nullptr, calendar_column, nullptr});
-        state.ResumeTiming();
+
         ASSERT_OK(CelonisTimeFunctions::in_calendar_prepare(ctx.get(),
                                                             FunctionContext::FunctionStateScope::FRAGMENT_LOCAL));
-        ASSERT_OK(
-                CelonisTimeFunctions::in_calendar_prepare(ctx.get(),
-                                                          FunctionContext::FunctionStateScope::THREAD_LOCAL));
+        ASSERT_OK(CelonisTimeFunctions::in_calendar_prepare(ctx.get(),
+                                                            FunctionContext::FunctionStateScope::THREAD_LOCAL));
+        state.ResumeTiming();
+
         EXPECT_TRUE(CelonisTimeFunctions::in_calendar(ctx.get(),
                                                       {timestamp_column, calendar_column, calendar_id_column}).ok());
+
+        state.PauseTiming();
         ASSERT_OK(CelonisTimeFunctions::in_calendar_close(ctx.get(),
                                                           FunctionContext::FunctionStateScope::THREAD_LOCAL));
-        ASSERT_OK(
-                CelonisTimeFunctions::in_calendar_close(ctx.get(),
-                                                        FunctionContext::FunctionStateScope::FRAGMENT_LOCAL));
+        ASSERT_OK(CelonisTimeFunctions::in_calendar_close(ctx.get(),
+                                                          FunctionContext::FunctionStateScope::FRAGMENT_LOCAL));
+        state.ResumeTiming();
     }
     state.counters["RowInvRate"] =
             benchmark::Counter(total_rows, benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
 }
 
-// Args: Number of rows / Number of calendar entries per id / Number of calendar ids
-BENCHMARK(BM_InCalendar)->ArgsProduct({{1000, 10000, 100000}, {10, 100, 1000}, {2, 4, 8}});
+// Args: Number of rows / Number of calendar entries (ignored for weekday) / Number of calendar ids / Use weekday calendar
+// Factory calendar benchmarks
+BENCHMARK(BM_InCalendar)->ArgsProduct({{1000, 10000, 100000}, {10, 100, 1000}, {2, 4, 8}, {0}});
+
+// Weekday calendar benchmarks (calendar entries parameter is ignored)
+BENCHMARK(BM_InCalendar)->ArgsProduct({{1000, 10000, 100000}, {0}, {2, 4, 8}, {1}});
 
 } // namespace starrocks
 
