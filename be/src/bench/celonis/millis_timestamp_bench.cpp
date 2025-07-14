a378b4f661d67f97736002e7058f5f24119426bf
Yaqi Zhang <y.zhang@celonis.com>
2025-07-14 13:58:03 -0500

    [Benchmark] Add benchmark of CELONIS_MILLIS_TIMESTAMP (#1794)
    ## Why I'm doing:
    
    We want to have a better understanding of the performance of
    CELONIS_MILLIS_TIMESTAMP.
    
    ## What I'm doing:
    
    This PR adds the benchmark of CELONIS_MILLIS_TIMESTAMP.
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [ ] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.3
      - [ ] 3.2
      - [ ] 3.1
      - [ ] 3.0
      - [ ] 2.5

---
+++ be/src/bench/celonis/millis_timestamp_bench.cpp
@@ -0,0 +1,77 @@
+#include <benchmark/benchmark.h>
+#include <gtest/gtest.h>
+
+#include <random>
+
+#include "column/column_helper.h"
+#include "exprs/anyval_util.h"
+#include "exprs/celonis/time_functions.h"
+#include "exprs/function_context.h"
+#include "runtime/types.h"
+#include "testutil/assert.h"
+
+/*
+2025-07-12T19:44:27+00:00
+Running ./be/build_Release/src/bench/celonis/output/millis_timestamp_bench
+Run on (32 X 3242.3 MHz CPU s)
+CPU Caches:
+  L1 Data 32 KiB (x16)
+  L1 Instruction 32 KiB (x16)
+  L2 Unified 512 KiB (x16)
+  L3 Unified 32768 KiB (x2)
+Load Average: 8.87, 4.83, 2.91
+// Number of rows
+------------------------------------------------------------------------------------
+Benchmark                          Time             CPU   Iterations UserCounters...
+------------------------------------------------------------------------------------
+BM_MillisTimestamp/1000         7589 ns         7568 ns        91711 RowInvRate=7.56815ns
+BM_MillisTimestamp/10000       66281 ns        66252 ns        10589 RowInvRate=6.62518ns
+BM_MillisTimestamp/100000     651890 ns       651875 ns         1082 RowInvRate=6.51875ns
+*/
+
+namespace starrocks {
+
+TimestampValue generate_random_timestamp() {
+    auto seed = 42;
+    std::mt19937_64 rng(seed);
+    // Define distribution range (Unix timestamps in milliseconds)
+    // This range covers from 1970 to ~2100
+    std::uniform_int_distribution<int64_t> dist(0, 4102444800000);
+    // Generate and return random timestamp
+    int64_t millis = dist(rng);
+    TimestampValue timestamp;
+    timestamp.from_unix_second(millis / 1000, millis % 1000 * 1000);
+    return timestamp;
+}
+
+static void BM_MillisTimestamp(benchmark::State& state) {
+    date::init_date_cache(); // This is needed for using TimestampValue.
+    int num_rows = state.range(0);
+
+    std::vector<FunctionContext::TypeDesc> arg_types = {
+            AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(TYPE_DATETIME))};
+    auto return_type = AnyValUtil::column_type_to_type_desc(TypeDescriptor::from_logical_type(TYPE_BIGINT));
+    std::unique_ptr<FunctionContext> ctx(FunctionContext::create_test_context(std::move(arg_types), return_type));
+
+    int total_rows = 0;
+    for (auto _ : state) {
+        state.PauseTiming();
+        total_rows += num_rows;
+        ColumnPtr input_column = ColumnHelper::create_column(TypeDescriptor(TYPE_DATETIME), true);
+        for (int i = 0; i < num_rows; i++) {
+            input_column->append_datum(generate_random_timestamp());
+        }
+        ctx->set_constant_columns({nullptr});
+        state.ResumeTiming();
+        EXPECT_TRUE(CelonisTimeFunctions::millis_timestamp(ctx.get(), {input_column}).ok());
+    }
+    state.counters["RowInvRate"] =
+            benchmark::Counter(total_rows, benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+}
+
+// Number of rows
+BENCHMARK(BM_MillisTimestamp)->ArgsProduct({{1000, 10000, 100000}});
+
+} // namespace starrocks
+
+BENCHMARK_MAIN();
