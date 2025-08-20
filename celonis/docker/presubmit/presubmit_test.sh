db013601c03975589e31f3d35e703ee9c66e576a
Ian Hu <ji.hu@celonis.com>
2025-08-19 20:55:38 -0400

    Run BE UT & Update gtest-parallel readme (#891)

--- celonis/docker/presubmit/presubmit_test.sh
+++ celonis/docker/presubmit/presubmit_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+# Make sure any single failure will fail the whole script
 set -e
 
 cd /celostar-starrocks;
@@ -10,10 +12,13 @@ cd /celostar-starrocks;
 #echo "./build.sh --be --clean -j `nproc`";
 #./build.sh --be --clean -j `nproc`;
 
-echo "Start to run FE UT"
-export FE_UT_PARALLEL=16;
-./run-fe-ut.sh;
+# Currently FE UT is taking 1+ hour due to installing jars and we don't do any development on it.
+# TODO: reinstall FE UT if necessary
+#echo "Start to run FE UT"
+#export FE_UT_PARALLEL=16;
+#./run-fe-ut.sh;
 
-echo "Start to run Celonis BE UT"
+# TODO: consolidate this after rebasing https://github.com/StarRocks/starrocks/pull/43991
+echo "Start to run BE UT"
 export LD_LIBRARY_PATH=/var/local/thirdparty/installed/lib
 ./run-be-ut.sh --use-staros --clean -j `nproc` --test "Celonis*"
---
+++ celonis/tools/gtest-parallel/README.md
@@ -0,0 +1,7 @@
+This tool is to run backend unit tests in parallel.
+
+```shell
+export GTEST_PARALLEL=gtest-parallel
+export PYTHON=python3
+../../../run-be-ut.sh
+```
