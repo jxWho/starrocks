de6745569617ba6c8278697898ef8db23c1c3cd8
Ian Hu <ji.hu@celonis.com>
2025-10-01 15:03:34 -0400

    [Rebase] use dev-env: 3.5.6-0930

--- docker/dockerfiles/allin1/buildAllin1Image.sh
+++ docker/dockerfiles/allin1/buildAllin1Image.sh
@@ -4,7 +4,7 @@ set -e
 
 RELEASE_VERSION=${RELEASE_VERSION:-"latest"}
 BUILD_TYPE=${BUILD_TYPE:-"Release"}
-BUILDER=${BUILDER:-"ghcr.io/celonis/celostar/starrocks-dev-env:pr1772-3.3-02ee3f6d-celo"}
+BUILDER=${BUILDER:-"ghcr.io/celonis/celostar/starrocks-dev-env:3.5.6-0930"}
 
 docker pull $BUILDER
 docker buildx build --force-rm=true --build-arg RELEASE_VERSION=${RELEASE_VERSION} --build-arg BUILD_TYPE=${BUILD_TYPE} --build-arg builder=${BUILDER} -f ../artifacts/artifact.Dockerfile -t starrocks-artifacts:${RELEASE_VERSION} ../../..
