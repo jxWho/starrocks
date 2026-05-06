2a52f173f8153c1898585848bbf0a761ac5c41ec
Ian Hu <ji.hu@celonis.com>
2026-05-06 16:19:47 -0400

    update dev-env: 3.5-cc-2548aa3d

--- docker/dockerfiles/allin1/buildAllin1Image.sh
+++ docker/dockerfiles/allin1/buildAllin1Image.sh
@@ -4,7 +4,7 @@ set -e
 
 RELEASE_VERSION=${RELEASE_VERSION:-"latest"}
 BUILD_TYPE=${BUILD_TYPE:-"Release"}
-BUILDER=${BUILDER:-"ghcr.io/celonis/celostar/starrocks-dev-env:3.5-94085107"}
+BUILDER=${BUILDER:-"ghcr.io/celonis/celostar/starrocks-dev-env:3.5-cc-2548aa3d"}
 DD_AGENT_VERSION=${DD_AGENT_VERSION:-"1.56.3"}
 
 docker pull $BUILDER
