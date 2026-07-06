fbc35048eb6762f143d8b3645cbea98713e0320f
Nick Weber <n.weber@celonis.com>
2026-07-06 09:10:48 +0200

    Remove more legacy Saola code (#2522)
    ## Why I'm doing:
    
    ## What I'm doing:
    
    Fixes #issue
    
    ## What type of PR is this:
    
    - [ ] BugFix
    - [ ] Feature
    - [ ] Enhancement
    - [x] Refactor
    - [ ] UT
    - [ ] Doc
    - [ ] Tool
    
    Does this PR entail a change in behavior?
    
    - [ ] Yes, this PR will result in a change in behavior.
    - [x] No, this PR will not result in a change in behavior.
    
    If yes, please specify the type of change:
    
    - [ ] Interface/UI changes: syntax, type conversion, expression
    evaluation, display information
    - [ ] Parameter changes: default values, similar parameters but with
    different default values
    - [ ] Policy changes: use new policy to replace old one, functionality
    automatically enabled
    - [x] Feature removed
    - [ ] Miscellaneous: upgrade & downgrade compatibility, etc.
    
    ## Checklist:
    
    - [ ] I have added test cases for my bug fix or my new feature
    - [ ] This pr needs user documentation (for new or modified features or
    behaviors)
      - [ ] I have added documentation for my new feature or new function
    - [ ] This is a backport pr
    
    ## Bugfix cherry-pick branch check:
    - [ ] I have checked the version labels which the pr will be
    auto-backported to the target branch
      - [ ] 3.5
      - [ ] 3.4
      - [ ] 3.3

--- be/src/exprs/celonis/modules/operators/process/bpmn/replay_result_source_target_fwd.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <memory>
-
-namespace celonis::accelerator::operators::process::bpmn {
-class replay_result_source_target;  // IWYU pragma: export
-using replay_result_source_target_t = std::shared_ptr<replay_result_source_target>;
-}  // namespace celonis::accelerator::operators::process::bpmn
